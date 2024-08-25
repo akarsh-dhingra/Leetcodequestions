e1cc393daca7a17bfae68c2c00b1058bba983658
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2024-08-25 10:39:00 +0530

    Time: 19 ms (5.22%), Space: 13.9 MB (7.58%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode*temp=head;
+        map<ListNode*,int>mpp;
+        while(temp!=NULL){
+            if(mpp.find(temp)!=mpp.end()){
+                return temp;
+                break;
+            }
+            mpp[temp]=1;
+            temp=temp->next;
+        }
+        return NULL;
+    }
+};
