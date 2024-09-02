f4ba9ba394f7cc96b9033f12daf6e4b6f9ccbf79
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2024-09-02 21:17:12 +0530

    Time: 10 ms (17.75%), Space: 16.1 MB (82.63%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,48 @@
 class Solution {
 public:
-int lower_bound(vector<int>&nums, int target, int n){
+int firstoccurence(vector<int>&nums, int target, int n){
     int low=0;
     int high=n-1;
-    int ans=n;
+    int first=-1;
     while(low<=high){
         int mid=low +(high-low)/2;
-        if(nums[mid]>=target){
-             ans=mid;
+        if(nums[mid]==target){
+             first=mid;
              high=mid-1;
+        }
+        else if(nums[mid]>target){
+               high=mid-1;
         }
         else{
             low=mid+1;
         }
+        }
+            return first;
+
     }
-    return ans;
-}
-int upper_bound(vector<int>&nums, int target,int n){
+int lastoccurence(vector<int>&nums, int target, int n){
     int low=0;
     int high=n-1;
-    int ans=n;
+    int last=-1;
     while(low<=high){
         int mid=low +(high-low)/2;
-        if(nums[mid]>target){
-             ans=mid;
-             high=mid-1;
+        if(nums[mid]==target){
+             last=mid;
+             low=mid+1;
+        }
+        else if(nums[mid]>target){
+               high=mid-1;
         }
         else{
             low=mid+1;
         }
+        }
+         return last;
     }
-    return ans;
-}
     vector<int> searchRange(vector<int>& nums, int target) {
      int n= nums.size();
-     int ub=upper_bound(nums,target,n);
-     int lb=lower_bound(nums,target,n);
-     if(lb==n || nums[lb]!=target){
-        return {-1,-1};
-     }
-     else {
-        return {lb,ub-1};
-     }
+     int fr=firstoccurence(nums,target,n);
+     int ls=lastoccurence(nums,target,n);
+     return {fr,ls};
 }
 };
