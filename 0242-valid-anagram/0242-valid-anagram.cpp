e38a122e1261f9647e37e232eeae780d0630d824
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2024-10-29 15:49:44 +0530

    Time: 0 ms (100%), Space: 9 MB (40.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // length check :
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-      if(s==t){
+        int hash[256]={};
+        for(int i=0;i<s.size();i++){
+              hash[s[i]]++;
+        }
+
+        for(int i=0;i<t.size();i++){
+              hash[t[i]]--;
+        }
+        for(int i=0;i<256;i++){
+            if(hash[i]!=0) return false ;
+        }
         return true;
-      }
-      else return false;
     }
 };
