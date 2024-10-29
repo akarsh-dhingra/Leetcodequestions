17352dd3fdd053f43645fa7a4e9ec13928377762
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2024-10-29 18:46:35 +0530

    Time: 0 ms (100%), Space: 12.5 MB (63.79%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
-int findlength(string s){
-   int i=0;
-   while(s[i]!='\0'){
-    i++;
-   }
-   return i;
-}
+// int findlength(string s){
+//    int i=0;
+//    while(s[i]!='\0'){
+//     i++;
+//    }
+//    return i;
+// }
     string removeDuplicates(string s) {
-        int len= findlength(s);
+        string ans={};
         int indx=0;
-        while(s[indx]!='\0'){
-            if(s[indx]==s[indx+1]){
-                s.erase(s.begin()+indx,s.begin()+indx+2);
-                indx=0;
-            }
-         else indx++;
+        while(indx<s.length()){
+            // same
+            // ans ka rightmost character and joh apna string ka currect character are same 
+          if(ans.length()>0&&ans[ans.length()-1]==s[indx]){
+            ans.pop_back();
+          }
+          else{
+            ans.push_back(s[indx]);
+          }
+          indx++;
         }
-        return s;
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int len= findlength(s);
+        // int indx=0;
+        // while(s[indx]!='\0'){
+        //     if(s[indx]==s[indx+1]){
+        //         s.erase(s.begin()+indx,s.begin()+indx+2);
+        //         indx=0;
+        //     }
+        //  else indx++;
+        // }
+        // return s;
     }
 };
