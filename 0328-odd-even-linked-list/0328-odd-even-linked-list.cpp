b5768b7ef73f1a77301e00e66249ec10e2a00383
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2024-12-22 09:27:04 +0530

    Time: 0 ms (100%), Space: 15.5 MB (35.41%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,33 +11,47 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL || head->next==NULL){
+        // if(head==NULL || head->next==NULL){
+        //     return head;
+        // }
+        // vector<int>v={};
+        // ListNode*temp=head;
+        // while(temp!=NULL&&temp->next!=NULL){
+        //     v.push_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // if(temp){
+        //     v.push_back(temp->val);
+        // }
+        // temp=head->next;
+        // while(temp!=NULL&&temp->next!=NULL){
+        //    v.push_back(temp->val);
+        //    temp=temp->next->next;
+        // }
+        // if(temp){
+        //     v.push_back(temp->val);
+        // }
+        // temp=head;
+        // int i=0;
+        // while(temp){
+        //     temp->val=v[i];
+        //     temp=temp->next;
+        //     i++;
+        // }
+        // return head;
+        if (head==NULL|| head->next==NULL){
             return head;
         }
-        vector<int>v={};
-        ListNode*temp=head;
-        while(temp!=NULL&&temp->next!=NULL){
-            v.push_back(temp->val);
-            temp=temp->next->next;
+        ListNode*odd=head;
+        ListNode*even=head->next;
+        ListNode*evenhead=head->next;
+        while(even!=NULL&&even->next!=NULL){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
         }
-        if(temp){
-            v.push_back(temp->val);
-        }
-        temp=head->next;
-        while(temp!=NULL&&temp->next!=NULL){
-           v.push_back(temp->val);
-           temp=temp->next->next;
-        }
-        if(temp){
-            v.push_back(temp->val);
-        }
-        temp=head;
-        int i=0;
-        while(temp){
-            temp->val=v[i];
-            temp=temp->next;
-            i++;
-        }
+        odd->next=evenhead;
         return head;
     }
 };
