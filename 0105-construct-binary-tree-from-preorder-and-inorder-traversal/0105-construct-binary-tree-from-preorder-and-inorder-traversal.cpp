ec05a63b1aecaf55a77beb4e123982f337498b50
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2025-01-05 09:09:29 +0530

    Time: 2 ms (81.68%), Space: 27.5 MB (42.21%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,65 +11,65 @@
  */
 class Solution {
 public:
-    // TreeNode* constructreefrominandpreordertraversal(vector<int>& preorder, vector<int>& inorder,int size,unordered_map<int, int> valueToIndexMap,int &preorderIndx, int inorderStart,int inorderend){
-    //     if(preorderIndx>=size||inorderStart>inorderend){
-    //         return NULL;
-    //     }
-    //     int element=preorder[preorderIndx];
-    //     preorderIndx++;
-    //     TreeNode*root= new TreeNode(element);
-    //        int elementpos= valueToIndexMap[element];
-    //     root->left=constructreefrominandpreordertraversal(preorder,inorder,size,valueToIndexMap,preorderIndx,inorderStart,elementpos-1);
-    //     root->right=constructreefrominandpreordertraversal(preorder,inorder,size,valueToIndexMap,preorderIndx,elementpos+1,inorderend);
-    //     return root;
-    // }
-    // TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-    //     int size=preorder.size();
-    //     int preorderIndx=0;
-    //     int inorderStart=0;
-    //     int inorderend=size-1;
-    // unordered_map<int, int> valueToIndexMap;
-    // for (int i = 0; i < size; i++) {
-    //     valueToIndexMap[inorder[i]] = i;
-    // }
-    //     TreeNode*root=constructreefrominandpreordertraversal(preorder,inorder,size,valueToIndexMap,preorderIndx,inorderStart,inorderend);
-    //     return root;
-    // }
-    TreeNode* constructTreeFromInAndPreOrder(vector<int>& preorder, vector<int>& inorder, 
-                                         int size, int& preorderIndex, 
-                                         int inorderStart, int inorderEnd, 
-                                         unordered_map<int, int>& valueToIndexMap) {
-    if (preorderIndex >= size || inorderStart > inorderEnd) {
-        return NULL;
+    TreeNode* constructreefrominandpreordertraversal(vector<int>& preorder, vector<int>& inorder,int size,unordered_map<int, int>&valueToIndexMap,int &preorderIndx, int inorderStart,int inorderend){
+        if(preorderIndx>=size||inorderStart>inorderend){
+            return NULL;
+        }
+        int element=preorder[preorderIndx];
+        preorderIndx++;
+        TreeNode*root= new TreeNode(element);
+           int elementpos= valueToIndexMap[element];
+        root->left=constructreefrominandpreordertraversal(preorder,inorder,size,valueToIndexMap,preorderIndx,inorderStart,elementpos-1);
+        root->right=constructreefrominandpreordertraversal(preorder,inorder,size,valueToIndexMap,preorderIndx,elementpos+1,inorderend);
+        return root;
     }
-
-    // Get the current root element and move the preorder index forward
-    int element = preorder[preorderIndex++];
-    TreeNode* root = new TreeNode(element);
-
-    // Get the position of the element in the inorder traversal using the map
-    int elementPos = valueToIndexMap[element];
-
-    // Recursive calls for left and right subtrees
-    root->left = constructTreeFromInAndPreOrder(preorder, inorder, size, preorderIndex, inorderStart, elementPos - 1, valueToIndexMap);
-    root->right = constructTreeFromInAndPreOrder(preorder, inorder, size, preorderIndex, elementPos + 1, inorderEnd, valueToIndexMap);
-
-    return root;
-}
-
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-    int size = preorder.size();
-    int preorderIndex = 0;
-    int inorderStart = 0;
-    int inorderEnd = size - 1;
-
-    // Create a map for the inorder traversal to quickly find element positions
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int size=preorder.size();
+        int preorderIndx=0;
+        int inorderStart=0;
+        int inorderend=size-1;
     unordered_map<int, int> valueToIndexMap;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++){
         valueToIndexMap[inorder[i]] = i;
     }
+        return constructreefrominandpreordertraversal(preorder,inorder,size,valueToIndexMap,preorderIndx,inorderStart,inorderend);
+    }
+//     TreeNode* constructTreeFromInAndPreOrder(vector<int>& preorder, vector<int>& inorder, 
+//                                          int size, int& preorderIndex, 
+//                                          int inorderStart, int inorderEnd, 
+//                                          unordered_map<int, int>& valueToIndexMap) {
+//     if (preorderIndex >= size || inorderStart > inorderEnd) {
+//         return NULL;
+//     }
 
-    // Call the recursive function
-    return constructTreeFromInAndPreOrder(preorder, inorder, size, preorderIndex, inorderStart, inorderEnd, valueToIndexMap);
-}
+//     // Get the current root element and move the preorder index forward
+//     int element = preorder[preorderIndex];
+//     preorderIndex++:
+//     TreeNode* root = new TreeNode(element);
+
+//     // Get the position of the element in the inorder traversal using the map
+//     int elementPos = valueToIndexMap[element];
+
+//     // Recursive calls for left and right subtrees
+//     root->left = constructTreeFromInAndPreOrder(preorder, inorder, size, preorderIndex, inorderStart, elementPos - 1, valueToIndexMap);
+//     root->right = constructTreeFromInAndPreOrder(preorder, inorder, size, preorderIndex, elementPos + 1, inorderEnd, valueToIndexMap);
+
+//     return root;
+// }
+
+// TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//     int size = preorder.size();
+//     int preorderIndex = 0;
+//     int inorderStart = 0;
+//     int inorderEnd = size - 1;
+
+//     // Create a map for the inorder traversal to quickly find element positions
+//     unordered_map<int, int> valueToIndexMap;
+//     for (int i = 0; i < size; i++) {
+//         valueToIndexMap[inorder[i]] = i;
+//     }
+
+//     // Call the recursive function
+//     return constructTreeFromInAndPreOrder(preorder, inorder, size, preorderIndex, inorderStart, inorderEnd, valueToIndexMap);
+// }
 };
