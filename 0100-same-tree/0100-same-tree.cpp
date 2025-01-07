7dafcec438cd219cad1bc9b9d1b26fc8ffae879d
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2025-01-07 08:27:02 +0530

    Time: 0 ms (100%), Space: 12.8 MB (32.26%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -50,9 +50,9 @@ public:
     if(p==NULL||q==NULL){
         return false;
     }
-    if(p->val!=q->val){
-        return false;
-    }
+    // if(p->val!=q->val){
+    //     return false;
+    // }
     if(p->val==q->val){
        bool a= isSameTree(p->left,q->left);
        bool b= isSameTree(p->right,q->right);
