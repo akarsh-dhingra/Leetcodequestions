7517f5c7e6dbcadc92c2b407a41206057fc22979
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2025-01-16 17:00:28 +0530

    Time: 3 ms (78.04%), Space: 28.4 MB (72.14%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
+        // vector<int>nge(n,-1);  // if not declared will give segmentation fault.
+        // if(n==1){
+        //     return nge;
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<=i+n-1;j++){  // circular array: we have to compare every element with all the element  in the array for that we need to make sure that window size is ==N all the time.
+        //     int indx=j%n;
+        //     if(nums[indx]>nums[i]){
+        //         nge[i]=nums[indx];
+        //         break;
+        //     }
+        //     }
+        // }
+        // return nge;
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        vector<int>nge(n,-1);  // if not declared will give segmentation fault.
+        stack<int>st;
+        vector<int>nge(n,-1);
         if(n==1){
             return nge;
         }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<=i+n-1;j++){  // circular array: we have to compare every element with all the element  in the array for that we need to make sure that window size is ==N all the time.
-            int indx=j%n;
-            if(nums[indx]>nums[i]){
-                nge[i]=nums[indx];
-                break;
+        for(int i=2*n-1;i>=0;i--){
+            int indx=i%n;
+            while(!st.empty()&&st.top()<=nums[i%n]){
+                st.pop();
             }
+            if(i<n){
+                nge[i]=st.empty()?-1:st.top();
             }
+            st.push(nums[indx%n]);
         }
         return nge;
     }
