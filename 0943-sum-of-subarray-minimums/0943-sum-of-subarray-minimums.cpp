31ecda7afc65e681f0200ba8b8b2b3716c9e7ae5
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2025-01-19 12:49:17 +0530

    Time: 22 ms (30.46%), Space: 47.3 MB (41.31%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,53 +1,42 @@
 class Solution {
 public:
-vector<int> nextsmallerelement(vector<int>& arr){
+vector<int>nextsmallerelement(vector<int>& arr){
     stack<int>st;
-    int n=arr.size();
-    vector<int> ans(n,-1);
+    // st.push(-1);
+        int n=arr.size();
+    vector<int>ans(n,-1);
     for(int i=n-1;i>=0;i--){
-        while(!st.empty()&&arr[st.top()]>arr[i]){
-        // Update with the next smaller index
-        st.pop();
+        while(!st.empty()&& arr[st.top()]>arr[i]){
+            st.pop();
         }
         ans[i]=st.empty()?n:st.top();
         st.push(i);
     }
     return ans;
 }
-vector<int> prevSmallerequ(vector<int> &a){
-    stack<int> st;
-    int n = a.size();
-    vector<int> ans(n, -1);  // Initialize with `-1` (no smaller element to the left)
-
-    for (int i = 0; i < n; i++) {
-        while (!st.empty() && a[st.top()] >= a[i]) {
+vector<int>prevsmallerele(vector<int>& arr){
+    stack<int>st;
+    int n=arr.size();
+    vector<int>ans(n,n);
+    for(int i=0;i<n;i++){
+        while(!st.empty()&&arr[st.top()]>=arr[i]){
             st.pop();
         }
-        ans[i] = st.empty() ? -1 : st.top();  // Update with the previous smaller index 
+        ans[i]=st.empty()?-1:st.top();
         st.push(i);
     }
     return ans;
 }
-
     int sumSubarrayMins(vector<int>& arr) {
         int n=arr.size();
-        // int sum=0;
         int modulo=(int)1e9+7;
-        // for(int i=0;i<n;i++){
-        //     int small=arr[i];
-        //     for(int j=i;j<n;j++){
-        //         small=min(small,arr[j]);
-        //         sum=(sum+small)%modulo;
-        //     }
-        // }
-        vector<int>nse=nextsmallerelement(arr);
-        vector<int>pse=prevSmallerequ(arr);
-        long long total=0;
+        vector<int>leftside=prevsmallerele(arr);
+        vector<int>rightside=nextsmallerelement(arr);
+        int total=0;
         for(int i=0;i<n;i++){
-            int left=i-pse[i];
-            int right=nse[i]-i;
-         
-        total = (total + (long long)left * right * arr[i] % modulo) % modulo;
+            int left=i-leftside[i];
+            int right=rightside[i]-i;
+            total=(total+(long long )left*right*arr[i]%modulo)%modulo;
         }
         return total;
     }
