65489bbb6d792814a8285b67328b452893711457
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2025-01-20 22:33:58 +0530

    Time: 35 ms (45.5%), Space: 94.2 MB (5.36%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,23 +1,33 @@
 class StockSpanner {
-    vector<int>ans;
+  stack<pair<int,int>>st;
+  int indx;
+  vector<int>ans;
 public:
     StockSpanner() {
-
+indx=-1;
     }
     
     int next(int price) {
     // i have to keep a track of all elements passing
     // stack<int>st;
     // st.push(price);
-    ans.push_back(price);
-    int cnt=1;
-    for(int i=ans.size()-2;i>=0;i--){
-    if(ans[i]<=price){
-    cnt+=1;
-    }
-    else break;
-    }
-return cnt;
+//     ans.push_back(price);
+//     int cnt=1;
+//     for(int i=ans.size()-2;i>=0;i--){
+//     if(ans[i]<=price){
+//     cnt+=1;
+//     }
+//     else break;
+//     }
+indx=indx+1;
+int actualans=0;
+while(!st.empty()&&st.top().first<=price){
+    st.pop();
+}
+int answ=indx-(st.empty()?-1:st.top().second);
+ans.push_back(answ);
+st.push({price,indx});
+return answ;
     }
 };
 
