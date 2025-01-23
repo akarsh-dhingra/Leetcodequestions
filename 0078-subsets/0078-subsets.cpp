a362c245dedcf97c2d5b346d29eecc9acd0008d1
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2025-01-23 08:52:03 +0530

    Time: 0 ms (100%), Space: 9.9 MB (62.25%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans;
-        int subs=1<<n;
-        vector<vector<int>> result;
-    // looping over all subsets from 0 to 2^n - 1 
-    for (int subset_mask=0;subset_mask<=subs-1;subset_mask++){
-           vector <int> subset;
-           for(int i=0;i<n;i++){
-                if (subset_mask & (1 << i)) {
-                subset.push_back(nums[i]);
-            }
-           }
-           result.push_back(subset);
+void f(int indx, vector<int>& current,vector<vector<int>>&result,vector<int>& nums){
+    if(indx>=nums.size()){
+        result.push_back(current);
+        return;
     }
-    return result;
+    current.push_back(nums[indx]);
+    f(indx+1,current,result,nums);
+
+    current.pop_back();
+    f(indx+1,current,result,nums);
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>result;
+        vector<int>current;
+        f(0,current,result,nums);
+        return result;
     }
 };
