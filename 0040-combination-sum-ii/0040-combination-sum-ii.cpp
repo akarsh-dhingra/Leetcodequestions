377193b0d1fec3c59741ecd18d590077986fe656
akarsh-dhingra <167018667+akarsh-dhingra@users.noreply.github.com>
2025-01-27 07:14:29 +0530

    Time: 0 ms (100%), Space: 13.9 MB (83.24%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-void combinations(int indx,vector<vector<int>>&result,vector<int>&ds,vector<int>& candidates, int target){
+void combinations(int indx,vector<vector<int>>&result,vector<int>&ds,vector<int>&arr, int target){
 if(target==0){
-result.push_back(ds);
-return;
+    result.push_back(ds);
+    return;
 }
-for(int i=indx;i<candidates.size();i++){
-if(i>indx&&candidates[i]==candidates[i-1]) continue;
-if(candidates[i]>target) break;
-ds.push_back(candidates[i]);
-combinations(i+1,result,ds,candidates,target-candidates[i]);
-ds.pop_back();
+for(int i=indx;i<arr.size();i++){
+    if(i>indx&&arr[i]==arr[i-1]) continue;
+    if(arr[i]>target) break;
+    ds.push_back(arr[i]);
+    combinations(i+1,result,ds,arr,target-arr[i]);
+    ds.pop_back();
 }
 }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
