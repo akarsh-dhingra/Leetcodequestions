f8a56e685539d53a44e2f26d47d13cf358d53a9a
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-04-19 11:38:53 +0530

    Time: 0 ms (100%), Space: 15.1 MB (95.22%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n= nums.size();
+int findMin(vector<int>& nums) {
+        int n=nums.size();
         int low=0;
         int high=n-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
-                    high =mid-1;
-                }
-                else {
-                    low=mid+1;
-                }
+            int prev=(mid+n-1)%n;
+            int next=(mid+1)%n;
+            if(nums[mid]<=nums[prev]&&nums[mid]<=nums[next]) return mid;
+            else if(nums[mid]>nums[high]){
+                low=mid+1;
             }
             else {
-                if(nums[mid]<=target && target<=nums[high]){
-                    low=mid+1;
-                }
-                else high =mid-1;
+                high=mid-1;
             }
         }
         return -1;
+    }
+    int bs(vector<int>& nums, int low,int high,int target){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) high=mid-1;
+            else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+       int n=nums.size();
+       int low=0;
+       int high=n-1;
+       int minelindx=findMin(nums);
+       int a=bs(nums,low,minelindx-1,target);
+       int b=bs(nums,minelindx,high,target);
+       if(a!=-1){
+        return a;
+       }
+       else if(b!=-1){
+        return b;
+       }
+       else{
+        return -1;
+       }
     }
 };
