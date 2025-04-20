c3ad2e151f4ae8690210ef8fb1f6a5ce6f564a16
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-04-20 23:47:06 +0530

    Time: 0 ms (100%), Space: 15 MB (95.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,49 +1,30 @@
 class Solution {
 public:
-int findMin(vector<int>& nums) {
-        int n=nums.size();
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int prev=(mid+n-1)%n;
-            int next=(mid+1)%n;
-            if(nums[mid]<=nums[prev]&&nums[mid]<=nums[next]) return mid;
-            else if(nums[mid]>nums[high]){
-                low=mid+1;
-            }
-            else {
-                high=mid-1;
-            }
-        }
-        return -1;
-    }
-    int bs(vector<int>& nums, int low,int high,int target){
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) high=mid-1;
-            else{
-                low=mid+1;
-            }
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
        int n=nums.size();
        int low=0;
        int high=n-1;
-       int minelindx=findMin(nums);
-       int a=bs(nums,low,minelindx-1,target);
-       int b=bs(nums,minelindx,high,target);
-       if(a!=-1){
-        return a;
+       while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target) return mid;
+        // left sorted 
+        if(nums[low]<=nums[mid]){
+            if(nums[low]<=target&&target<=nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        if(nums[high]>=nums[mid]){
+            if(nums[high]>=target&&target>=nums[mid]){
+              low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
        }
-       else if(b!=-1){
-        return b;
-       }
-       else{
-        return -1;
-       }
+    return -1;
     }
 };
