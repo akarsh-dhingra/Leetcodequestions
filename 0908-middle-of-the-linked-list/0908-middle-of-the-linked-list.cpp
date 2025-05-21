6def3cfed690366896e56fd9fbdf1c072cdbe7b2
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-05-21 11:57:58 +0530

    Time: 0 ms (100%), Space: 10 MB (63.17%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -13,8 +13,7 @@ public:
     ListNode* middleNode(ListNode* head) {
            ListNode*slow=head;
            ListNode*fast=head;
-           // in case of even fast==null and in case of odd fast->next==null
-           while(fast!=nullptr && fast->next!=NULL){
+           while(fast!=NULL&&fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
            }
