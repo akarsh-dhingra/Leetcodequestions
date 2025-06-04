ee3cc9b6b29ae6726edc098558a0e100254269f9
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-04 13:46:29 +0530

    Time: 3 ms (81.33%), Space: 13.7 MB (65.46%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -5,18 +5,23 @@ public:
         // step 2: delete those and use this string to repeat step 1
         // step 3: return
         // edge case if no adjacent duplicates
-
+        // stack sa 
+        // string ko built krte hai from scratch -> Pattern
         // find 
         int len=s.length();
         string ans;
         int indx=0;
         while(indx < s.length()){
-            if(s[indx]==s[indx+1]){
-                s.erase(indx,2);
-                indx=0;
-            }
-          else indx++;
+           // same
+           // ans ka rightmost char delete krdenge and string s ka curr char
+           if(ans.length()>0&&ans[ans.length()-1]==s[indx]){
+            ans.pop_back();
+           }
+          else {
+            ans.push_back(s[indx]);
+          }
+          indx++;
         }
-        return s;
+        return ans;
     }
 };
