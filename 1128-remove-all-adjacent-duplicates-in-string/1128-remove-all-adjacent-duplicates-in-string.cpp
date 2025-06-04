e0d2ad0186c83666f12d25dc6738ba4d586b30b7
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-04 13:08:43 +0530

    Time: 2766 ms (5.08%), Space: 12.5 MB (95.85%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
-// int findlength(string s){
-//    int i=0;
-//    while(s[i]!='\0'){
-//     i++;
-//    }
-//    return i;
-// }
     string removeDuplicates(string s) {
-        string ans={};
+        // step 1:choosing any adjacent and two equal letters indx and indx+1
+        // step 2: delete those and use this string to repeat step 1
+        // step 3: return
+        // edge case if no adjacent duplicates
+
+        // find 
+        int len=s.length();
+        string ans;
         int indx=0;
-        while(indx<s.length()){
-            // same
-            // ans ka rightmost character and joh apna string ka currect character are same 
-          if(ans.length()>0&&ans[ans.length()-1]==s[indx]){
-            ans.pop_back();
-          }
-          else{
-            ans.push_back(s[indx]);
-          }
-          indx++;
+        while(s[indx]!='\0'){
+            if(s[indx]==s[indx+1]){
+                s.erase(indx,2);
+                indx=0;
+            }
+          else indx++;
         }
-        return ans;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int len= findlength(s);
-        // int indx=0;
-        // while(s[indx]!='\0'){
-        //     if(s[indx]==s[indx+1]){
-        //         s.erase(s.begin()+indx,s.begin()+indx+2);
-        //         indx=0;
-        //     }
-        //  else indx++;
-        // }
-        // return s;
+        return s;
     }
 };
