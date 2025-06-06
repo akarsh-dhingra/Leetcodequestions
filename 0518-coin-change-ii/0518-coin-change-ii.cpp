4962d781c36b8bcffe38ad32c3c7020a6389fb29
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-06 20:56:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+ int subsetsum(vector<int>&coins,int amount,int n){
+    vector<vector<int>> t(n + 1, vector<int>(amount + 1, 0));
+  for(int i = 0; i < n + 1; i++) {
+        t[i][0] = 1; // Only base case needed
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<amount+1;j++){
+            if(coins[i-1]<=j){
+                t[i][j]=t[i-1][j]+t[i][j-coins[i-1]];
+            }
+            else t[i][j]=t[i-1][j];
+        }
+    }
+    return t[n][amount];
+ }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        int noofways=subsetsum(coins,amount,n);
+        return noofways;
+    }
+};
