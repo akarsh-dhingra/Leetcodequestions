a4122815fddb314670ce8b770659f49f69681ba6
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-06 15:16:17 +0530

    Time: 3 ms (71.92%), Space: 22 MB (92.79%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-bool checkpalindrome(string k, int start,int end){
-  while(start<end){
-    if(k[start]==k[end]){
-        start++;
-        end--;
+bool checkpalindrome(string&ch,int start,int end){
+    // t.c=0(len)
+    while(start<end){
+        if(ch[start]==ch[end]){
+            start++;
+            end--;
+        }
+        else{
+            return false;
+        }
     }
-    else return false;
-  }
-  return true;
+    return true;
 }
     bool validPalindrome(string s) {
-        int i=0;
-        int j= s.length()-1;
-        while(i<j){
-            if(s[i]==s[j]){
-            i++;
-            j--;
+        if(checkpalindrome(s,0,s.length())) return true;
+        int low=0;
+        int high=s.size()-1;
+        while(low<=high){
+            if(s[low]==s[high]){
+                swap(s[low],s[high]);
+                low++;
+                high--;
             }
             else{
                 // s[i]!=s[j]
                 // 1 removal allowed
-                // check palindrome for string after removal.
-                bool ans1=checkpalindrome(s,i+1,j);
-                bool ans2=checkpalindrome(s,i,j-1);
-                 return ans1 || ans2;
+                // check palindrome for remaining string after removal. 
+                bool ans1=checkpalindrome(s,low+1,high);
+                bool ans2=checkpalindrome(s,low,high-1);
+                return ans1||ans2;
             }
         }
-        // agar yahan tk pohoche ho yaahni valid palindrome
-        // yahni 0 removals
-    // agar palindrome aapka valid hai toh uss particular case mai aap 
-    return true;
+        return true;
     }
 };
