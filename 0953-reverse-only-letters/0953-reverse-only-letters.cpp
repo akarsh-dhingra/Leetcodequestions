abcb60430d75ad689fa943402436d4b34d02b346
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-06 11:56:22 +0530

    Time: 0 ms (100%), Space: 7.9 MB (82.47%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-void reversedd(string s){
-    int low=0;
-    int high=s.size()-1;
-    while(low<high){
-       if(isalpha(s[low])==true && isalpha(s[high])==true) {
-        swap(s[low],s[high]);
-        low++;
-        high--;
-    }
-    else if(isalpha(s[low])==false){
-        low++;
-    }
-    else high--;
-    }
-}
     string reverseOnlyLetters(string s) {
-    int low=0;
-    int high=s.size()-1;
-    while(low<high){
-       if(isalpha(s[low]) && isalpha(s[high])) {
-        swap(s[low],s[high]);
-        low++;
-        high--;
+        int low=0;
+        int high=s.size()-1;
+        while(low<high){
+if(isalpha(s[low])&&isalpha(s[high])){
+            swap(s[low],s[high]);
+            low++;
+            high--;
+}
+else if(!isalpha(s[low])){
+    low++;
+}
+else{
+    high--;
+}
     }
-    else if(!isalpha(s[low])){
-        low++;
-    }
-    else high--;
-    }
-        return s;
-    }
+    return s;}
 };
