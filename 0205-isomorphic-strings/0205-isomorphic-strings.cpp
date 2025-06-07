d6117afd79da2c499c697842f05a807792cff1b9
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-07 17:06:27 +0530

    Time: 0 ms (100%), Space: 9.5 MB (9.79%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int a=s.size();
-        int hashans[256]={0};
-        bool istcharmapped[256]={0};
+       vector<bool>tseen(256,0);
+
+       vector<int>hash(256,0);
+
        for(int i=0;i<s.size();i++){
-        if(hashans[s[i]]==0 && istcharmapped[t[i]]==0){
-            hashans[s[i]]=t[i];
-            istcharmapped[t[i]]=1;
-        }
+       if(hash[s[i]]==0&&!tseen[t[i]]){
+       hash[s[i]]=t[i];
+       tseen[t[i]]=true;
+       }   
        }
-       for(int i=0;i<t.size();i++){
-             if(char(hashans[s[i]])!=t[i] ){
-                return false;
-             }
+       string ans="";
+       for(int i=0;i<s.size();i++){
+ans.push_back(hash[s[i]]);
        }
-       return true;
+       if(ans==t) return true;
+       else return false;
     }
 };
