e5e501cf00f764cbb21f4c26793640fdb9be0ff5
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-07 11:28:39 +0530

    Time: 121 ms (46.06%), Space: 205.8 MB (6.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool isSubsetSum(vector<int>& arr, int sum) {
-        int n=arr.size();
-       vector<vector<int>> t(n + 1, vector<int>(sum + 1));
-        
-       for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-               if(i==0) t[i][j]= false;
-               if(j==0) t[i][j]=true;
-            }
+
+  bool isSubsetSumrec(vector<int>& arr,int n,int sum){
+    vector<vector<double>>t(n+1,vector<double>(sum+1,false));
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(j==0)  t[i][j]=true;
         }
-        for(int i=1;i<n+1;i++){
-        for (int j = 1; j <sum+1; j++) {
-           if(arr[i-1]<=j){
-                    t[i][j]=t[i-1][j-arr[i-1]]||t[i-1][j];
-                }
-                else t[i][j]=t[i-1][j];
-            }
-        }
-        return t[n][sum];
     }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(arr[i-1]<=j){
+                t[i][j]=t[i-1][j]||t[i-1][j-arr[i-1]];
+            }
+            else t[i][j]=t[i-1][j];
+        }
+
+    }
+    return t[n][sum];
+  }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
@@ -30,7 +29,7 @@ public:
             return false;
         }
         else{
-            return isSubsetSum(nums,sum/2);
+            return isSubsetSumrec(nums,n,sum/2);
         }
     }
 };
