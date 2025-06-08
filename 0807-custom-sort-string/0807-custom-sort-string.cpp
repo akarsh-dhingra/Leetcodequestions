54b571ed9683aa7b0bafc24160fe9c869675fb65
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-08 12:43:24 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.62%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-static string str;
-static bool compare(char chara,char charb){
-return (str.find(chara)<str.find(charb));
-}
+   // custom comparator
+   static string str;
+   static bool compare(char&a,char&b){
+        return str.find(a)<str.find(b);
+    }
     string customSortString(string order, string s) {
+      // order has been sorted in some unique order
+      // previously and s
       str=order;
       sort(s.begin(),s.end(),compare);
       return s;
     }
-};
-string Solution::str;
+}; string Solution::str;
