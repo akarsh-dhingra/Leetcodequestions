fd49ffe4e9593ae44ab4a79a2f222e60ad72e5f8
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-09 22:22:20 +0530

    Time: 2 ms (89.35%), Space: 14.3 MB (45.87%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        // If pair mai push krunga toh kabhi bhi jab stack ko empty krna paddega
-        // toh dikkat nahi aayegi
-
-        stack<pair<char,int>>st;
-
-        for(char c:s){
-            if(!st.empty()&&st.top().first==c){
-                st.top().second++;
-            }
-            else{
-                st.push({c,1});
-            }
-            if(st.top().second==k){
-                st.pop();
-            }
+       stack<pair<char,int>>st;
+       for(char c:s){
+        if(!st.empty()&&st.top().first==c){
+            st.top().second++;
         }
-
+        else st.push({c,1});
+        if(st.top().second==k) st.pop();
+       }
         string res="";
         while(!st.empty()){
             res.append(st.top().second,st.top().first);
