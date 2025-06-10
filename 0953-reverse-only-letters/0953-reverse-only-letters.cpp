a0080db55b813f027a24ecb8f6d961cf7c8574fd
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2025-06-10 11:10:09 +0530

    Time: 0 ms (100%), Space: 8.1 MB (53.95%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
+bool checkchar(char c){
+    int x=(char)c;
+    if(x>=65&&x<=90||x>=97&&x<=122){
+        return true;
+    }
+    else return false;
+}
     string reverseOnlyLetters(string s) {
-        int low=0;
-        int high=s.size()-1;
-        while(low<high){
-if(isalpha(s[low])&&isalpha(s[high])){
+       int low=0;
+       int high=s.size()-1;
+       while(low<=high){
+        if(checkchar(s[low])&&checkchar(s[high])){
             swap(s[low],s[high]);
             low++;
             high--;
-}
-else if(!isalpha(s[low])){
-    low++;
-}
-else{
-    high--;
-}
+        }
+        else if(!checkchar(s[low])){
+            low++;
+        }
+        else {
+            high--;
+        }
+       }
+
+    return s;
     }
-    return s;}
 };
