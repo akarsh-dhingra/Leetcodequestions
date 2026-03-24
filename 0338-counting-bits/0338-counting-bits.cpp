013187d1ef48cddc20300773b36c29582fff7aba
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-03-24 15:08:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int countSetBits(int n){
-        int count=0;
-        while(n>0){
-            if(n&1) count++;
-            n=n>>1;
-        }
-        return count;
-    }
+    // int countSetBits(int n){
+    //     int count=0;
+    //     while(n>0){
+    //         if(n&1) count++;
+    //         n=n>>1;
+    //     }
+    //     return count;
+    // }
+    // int countSetBits2(int x){
+
+    // }
     vector<int> countBits(int n) {
         // If I get the no. of set bits for each of the number between 1 to n then 
         // I can evenutally get my answer
-    vector<int>ans;
+    vector<int>ans(n+1,0);
 
-        for(int i=0;i<=n;i++){
-            int x=countSetBits(i);
-            ans.push_back(x);
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+
+                ans[i]=ans[i/2];
+            }
+            else{
+                ans[i]=ans[i/2]+1;
+            }
         }
         return ans;
 
