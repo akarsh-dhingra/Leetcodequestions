29c4d39029885c9d41a5b137dcc9731e90588dfc
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-03-24 15:00:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    int countSetBits(int n){
+        int count=0;
+        while(n>0){
+            if(n&1) count++;
+            n=n>>1;
+        }
+        return count;
+    }
     vector<int> countBits(int n) {
         // If I get the no. of set bits for each of the number between 1 to n then 
         // I can evenutally get my answer
     vector<int>ans;
 
         for(int i=0;i<=n;i++){
-            int x=__builtin_popcount(i);
+            int x=countSetBits(i);
             ans.push_back(x);
         }
         return ans;
