ca9523df2d40d9bd3d2a4fea3ef59acad42e6722
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-03-28 16:20:11 +0530

    Time: 48 ms (38.01%), Space: 8.2 MB (36.54%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+long long M=1e9+7;
+    int concatenatedBinary(int n) {
+        long result=0;
+        
+        for(int i=1;i<=n;i++){
+            int digits=log2(i)+1;
+            result=((result<<digits)%M+i)%M;
+        }
+        return result;
+    }
+};
