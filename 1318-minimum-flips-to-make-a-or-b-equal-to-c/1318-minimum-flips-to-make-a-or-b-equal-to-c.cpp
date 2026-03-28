e1f35c8c272333d8de013c27b02fc19afae1a84e
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-03-28 12:21:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,6 +3,6 @@ public:
     int minFlips(int a, int b, int c) {
         int result=(a|b)^c;
         int result2=(a&b);
-        return __builtin_popcount(result)+__builtin_popcount(result2&result);
-    }
+        return __builtin_popcount(result)+__builtin_popcount(result2);
+    }w
 };
