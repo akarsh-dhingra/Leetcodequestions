3431af8b024fcc69d3a6aa561a3603fcfb270f5d
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-03-28 19:38:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(int n) {
         int cnt=0;
         while(n!=0){
-            n=n & n-1;
-            cnt++;
+            if(n%2==1)cnt++;
+            n=n/2;
         }
         return cnt;
     }
