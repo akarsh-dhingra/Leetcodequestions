b99d59cd6b4b64f819ae9150760ecaae494f2ddd
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-04-06 13:20:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,43 @@
 class Solution {
 public:
-    void bfss(int row, int col, vector<vector<char>>& grid,vector<vector<bool>>&vis){
-        vis[row][col]=1;
+    void solveBfs(vector<vector<bool>>&vis,vector<vector<char>>& grid,int n,int m,int curRow,int curCol){
         queue<pair<int,int>>que;
-        que.push({row,col});
-        int n=grid.size();
-        int m=grid[0].size();
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
+        que.push({curRow,curCol});
+        vis[curRow][curCol]=true;
+
+        int deltaRow[] = {-1, 0, 1, 0};
+        int deltaCol[] = {0, 1, 0, -1};
 
         while(!que.empty()){
-            int row1=que.front().first;
-            int col1=que.front().second;
+            int topR=que.front().first;
+            int topC=que.front().second;
+
             que.pop();
-
-            // Now i have to traverse in its neighbours now how to get tot it's negihbours
-
-            for(int i=0;i<4;i++ ){
-               int nRow = row1 + delRow[i];
-               int nCol = col1 + delCol[i];
-                        
-                    if(nRow>=0&&nRow<n&&nCol>=0&&nCol<m
-                    &&grid[nRow][nCol]=='1'&&!vis[nRow][nCol]){
-                            vis[nRow][nCol]=true;
-                            que.push({nRow,nCol});
-                    }
-                }
+            for(int i=0;i<4;i++){
+                int nRow=topR+deltaRow[i];
+                int nCol=topC+deltaCol[i];
+        if(nRow>=0&&nRow<n&&nCol>=0&&nCol<m && vis[nRow][nCol]==false && grid[nRow][nCol]=='1' ){
+            que.push({nRow,nCol});
+            vis[nRow][nCol]=true;
+        }
             }
-        return;
+        }
     }
+    
     int numIslands(vector<vector<char>>& grid) {
-        // I have to first conver the char into 
         int n=grid.size();
         int m=grid[0].size();
-
+        
         vector<vector<bool>>vis(n,vector<bool>(m,false));
-        int cnt=0;
-        for(int row=0;row<n;row++){
-            for(int col=0;col<m;col++){
-                if(!vis[row][col]&& grid[row][col]=='1'){
-                    bfss(row,col,grid,vis);
-                    cnt++;
+        int islands=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==false && grid[i][j]=='1'){
+                    islands++;
+                    solveBfs(vis,grid,n,m,i,j);
                 }
             }
         }
-        return cnt;
+        return islands;
     }
 };
