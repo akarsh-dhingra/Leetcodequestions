29f6d370b02433cdfd9971e2d3b2be22d6f8e2cc
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-04-07 08:14:13 +0530

    Time: 0 ms (100%), Space: 16.9 MB (47.03%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        // Apply BFS+ flood Fill
+
+        int n=grid.size();
+        int m=grid[0].size();
+
+        // Create a visited arr
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        // Create a queue becuase it's bfs
+        queue<pair<pair<int,int>,int>>que;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    que.push({{i,j},0});
+                    vis[i][j]=2;
+                }
+            }
+        }
+
+        // Now move forward
+        int tMax=0;
+        int deltaRow[]={0,-1,0,1};
+        int deltaCol[]={-1,0,1,0};
+
+        while(!que.empty()){
+            int r=que.front().first.first;
+            int c=que.front().first.second;
+            int t=que.front().second;
+            que.pop();
+            tMax=max(tMax,t);
+
+            for(int i=0;i<4;i++){
+                int nRow=r+deltaRow[i];
+                int nCol=c+deltaCol[i];
+
+                if(nRow>=0&& nCol>=0&& nRow<n && nCol<m&& vis[nRow][nCol]!=2&&grid[nRow][nCol]==1){
+                    vis[nRow][nCol]=2;
+                    que.push({{nRow,nCol},t+1});
+                }
+            }
+
+        }
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(vis[i][j]!=2 && grid[i][j]==1) return -1;
+        return tMax;
+    }
+};
