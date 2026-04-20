257983c1412b690181ba8ee9fea28e0453180f66
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-04-20 21:06:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-            // Let's creata an adjacency matrix or graph first
-            vector<vector<pair<int,int>>>adj(n);
+// Dijsktarta
+// stops=(src,dest);
+// In standard dijsktara my approach was that I used to think 
+// in terms of {d,node} but here I"ll think yk in terms of {d,n,stops}
+//  I cannot apply dijsktara algo here because yk I may end up 
+//  for that matter taking a smaller dist but that may come at a tradeoff of more number of stops which will violate my condition given in the problem 
+// hence my criteria for judgement is not primarilyy 
+// dist but the stops as well
+    vector<vector<pair<int,int>>>adj(n);
+    for(auto it:flights){
+        adj[it[0]].push_back({it[1],it[2]});
+    }
+    queue<pair<int,pair<int,int>>>que;
+    que.push({0,{src,0}});
+    vector<int>dist(n,INT_MAX);
+    dist[src]=0;
 
-            for(auto it:flights){
-                int sNode=it[0];
-                int eNode=it[1];
-                int eWt=it[2];
-                adj[sNode].push_back({eNode,eWt});
-            }
-            queue<pair<int,pair<int,int>>>que;
-            que.push({0,{src,0}});
-            vector<int>dist(n,INT_MAX);
-            dist[src]=0;
+    while(!que.empty()){
+        auto it=que.front();
+        int stops=it.first;
+        int node=it.second.first;
+        int dis=it.second.second;
 
-            while(!que.empty()){
-              auto it=que.front();
-              que.pop();
-              int stops=it.first;
-              int node=it.second.first;
-              int cost=it.second.second;
+        que.pop();
+        if(stops>k) continue;
+            for(auto it:adj[node]){
+                int padosi=it.first;
+                int edw=it.second;
 
-              if(stops>k) continue;  
-                for(auto it:adj[node]){
-                    int padosi=it.first;
-                    int edw=it.second;
-
-                    if(edw+cost<dist[padosi] && stops<=k){
-                        dist[padosi]=edw+cost;
-                        que.push({stops+1,{padosi,edw+cost}});
-                    }
+                if(dis+edw<dist[padosi] && stops<=k ){
+                        dist[padosi]=edw+dis;
+                        que.push({stops+1,{padosi,edw+dis}});
                 }
             }
+        }
 
-            if(dist[dst]==INT_MAX) return -1;
-            return dist[dst];
-            
+    if(dist[dst]==INT_MAX) return INT_MAX;
+    return dist[dst];
+
     }
 };
