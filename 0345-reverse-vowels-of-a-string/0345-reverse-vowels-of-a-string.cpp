b1398ee7e49e4a2312b22c5c2f531576e8e928a3
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-05-28 15:08:05 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.6%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         if(x=='e'||x=='i'||x=='o'||x=='u'||x=='a'||x=='A'||x=='E'||x=='I'||x=='U'||x=='O'){
             return true;
         }
-       else return false;
+        return false;
     }
     string reverseVowels(string s) {
         // check vowels and reverse them where they 
@@ -12,17 +12,16 @@ public:
         int l=0;
         int r=s.size()-1;
 
-        while(l<=r){
+        while(l<r){
             if(checkVowel(s[l])&&checkVowel(s[r])){
                 swap(s[l],s[r]);
                 l++;
                 r--;
             }
-            
-        else if(checkVowel(s[r])) l++;
-        else{
-            r--;
-        }
+            else if(!checkVowel(s[l])) l++;
+            else{
+                r--;
+            }
         }
         return s;
     }
