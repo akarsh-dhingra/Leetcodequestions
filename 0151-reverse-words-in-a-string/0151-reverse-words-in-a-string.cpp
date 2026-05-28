cb2a889638ba70ad5dfa0337a97cf607bc542e57
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-05-28 18:32:24 +0530

    Time: 4 ms (22.75%), Space: 10.2 MB (62.12%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
-        string token;
-        string result="";
-        while(ss>>token){
-            result=token+" "+result;
+        int l=0;
+        int r=0;
+        int i=0;
+int n=s.size();
+        reverse(s.begin(),s.end());
+        while(i<n){
+            while(i<n&& s[i]!=' '){
+                s[r++]=s[i++];
+            }
+            if(l<r){
+                reverse(s.begin()+l,s.begin()+r);
+                s[r]=' ';
+                r++;
+                l=r;
+            }
+        i++;
         }
-
-        return result.substr(0,result.size()-1);
+        return s.substr(0,r-1);
     }   
 };
