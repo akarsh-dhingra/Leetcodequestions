8954381db0c51e5242c81b6ef978f53060ecc380
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-05-29 17:32:17 +0530

    Time: 2 ms (56.31%), Space: 65.1 MB (95.89%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -3,12 +3,11 @@ public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int i=0;
         int j=0;
-        int n=nums.size();
-        int product=1;
         int subArr=0;
+        int product=1;
+        int n=nums.size();
         if(k<=1) return 0;
         while(j<n){
-            // every time j++ j-i+1 will get added
             product*=nums[j];
             while(product>=k){
                 product/=nums[i];
@@ -17,6 +16,12 @@ public:
             subArr+=(j-i+1);
             j++;
         }
+    // since both i and j pointer moved only towards the right and never 
+    // moved backward hene there is not a chance even that the T.C: can be 
+    // O(n^2) because that's only possible when the pointer will mov
+    // backward since that never happened hence the t.c is o(n^2)
+
+
         return subArr;
     }
 };
