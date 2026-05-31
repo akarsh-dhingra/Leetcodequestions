7e7bfe807efdde09246a6f47d1dd2158bfc4e559
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-05-31 10:04:58 +0530

    Time: 3 ms (45.42%), Space: 9.7 MB (48.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         sort(s.begin(),s.end());
-         sort(t.begin(),t.end());
-         return s==t;
+            // if at the end hash table is empty 
+            // that means both are anagrams of each other
+
+            vector<int>hash(26,0);
+
+            for(int i=0;i<s.size();i++){
+                hash[s[i]-'a']++;
+            }
+            for(int i=0;i<t.size();i++){
+                hash[t[i]-'a']--;
+            }
+
+            for(auto it:hash){
+                if(it!=0) return false;
+            }
+            return true;
     }
 };
