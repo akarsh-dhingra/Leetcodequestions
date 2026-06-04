40e0dd2534d00189e0921bde866cfc965ee36b68
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-04 23:41:44 +0530

    Time: 446 ms (10.07%), Space: 118.8 MB (14.74%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int waviness(int x) {
-        vector<int> d;
+    int waviness(int x){
+        int cnt=0;
+        vector<int>d;
 
-        while (x > 0) {
-            d.push_back(x % 10);
-            x /= 10;
+        while(x>0){
+            d.push_back(x%10);
+            x/=10;
         }
 
-        int n = d.size();
-        if (n < 3) return 0;
-
-        int cnt = 0;
-
-        for (int i = 1; i < n - 1; i++) {
-            if ((d[i] > d[i - 1] && d[i] > d[i + 1]) ||
-                (d[i] < d[i - 1] && d[i] < d[i + 1])) {
+        if(d.size()<3) return 0;
+        
+        for(int i=1;i<d.size()-1;i++){
+            if((d[i]>d[i+1]&& d[i]>d[i-1])||(d[i]<d[i-1]&&d[i]<d[i+1])){
                 cnt++;
             }
         }
-
         return cnt;
     }
-
     int totalWaviness(int num1, int num2) {
         int cnt=0;
         for(int i=num1;i<=num2;i++){
