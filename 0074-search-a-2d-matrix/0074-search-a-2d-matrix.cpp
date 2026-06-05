a1c971b00a1c273d10cd45f109546abcd6d0c6a6
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-05 09:35:36 +0530

    Time: 0 ms (100%), Space: 13.2 MB (77.78%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
+    bool BS(int low,int high,vector<int>&arr,int target){
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target) return true;
+            if(arr[mid]<target){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        return false;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         // 1st approahc 
      int rows=matrix.size();  // m
      int cols=matrix[0].size(); // n
+        for(auto it:matrix[0]){
+                cout<<it<<" ";
+        }
         for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(matrix[i][j]==target) return true;
-            }
+           if(matrix[i][0]<=target && target<=matrix[i][cols-1]){
+           return BS(0,cols-1,matrix[i],target);  // will be called once and hence T.c: O(m+logn)
+           }
         }
         return false;
     }
