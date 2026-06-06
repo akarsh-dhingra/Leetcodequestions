6196fe189d5e052ff35c2ade0909f18095569ba6
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-06 21:05:54 +0530

    Time: 1 ms (26.2%), Space: 9.6 MB (28.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int cnt=0;
+    
     int solveUsingRec(int i,int j){
-        if(i==0||j==0) return 1;
+        if(i==0&&j==0) return 1;
         if(i<0 || j<0) return 0;
-
-        int up=solveUsingRec(i-1,j);
+        int top=solveUsingRec(i-1,j);
         int left=solveUsingRec(i,j-1);
-        return up+left;
-    }
-    int solveUsingMem(int i,int j, vector<vector<int>>&dp){
-        if(i==0||j==0) return dp[i][j]=1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=solveUsingMem(i-1,j,dp);
-        int left=solveUsingMem(i,j-1,dp);
-        dp[i][j]=up+left;
-        return dp[i][j];
-    }
 
+        return top+left;
+    }
+    int solveUsingTab(int m,int n){
+        vector<vector<long long>>dp(m+1,vector<long long >(n+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++) dp[0][i]=1;
+        for(int i=1;i<=m;i++) dp[i][0]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
     int uniquePaths(int m, int n) {
-        // Move down 
-        // Move right 
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solveUsingMem(m-1,n-1,dp);
+        return solveUsingTab(m,n);
     }
 };
