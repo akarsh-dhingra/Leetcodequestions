70a04de6fb50ffce1064d5d711dbacf6f1eb4e12
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-09 10:50:26 +0530

    Time: 3 ms (28.6%), Space: 97.3 MB (59.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-    //    [7,1,5,3,6,4]
-        // Now we've to maximise
-        // choose a single day to buy one stock and 
-        // different day to sell one 
-        // Calculate Profit
         int n=prices.size();
-        int maxProfit=0;
+        int initProfit=0;
         int mini=prices[0];
+        // If I buy of a particular day then I will sell it on ith day
         for(int i=1;i<n;i++){
-            int cost=prices[i]-mini;
-            maxProfit=max(maxProfit,cost);
-            mini=min(mini,prices[i]);
-            }
-            return maxProfit;   
+            int diff=prices[i]-mini;  // because I will always buy on the mini day
+            initProfit=max(initProfit,diff);
+            mini=min(prices[i],mini);
         }
+        return initProfit;
+    }
 };
