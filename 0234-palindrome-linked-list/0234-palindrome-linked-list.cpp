993d082a70915020349044e11f7cebd96a16bb92
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-12 14:22:40 +0530

    Time: 2 ms (63.39%), Space: 118.1 MB (64.04%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,21 +10,44 @@
  */
 class Solution {
 public:
+    ListNode* findMid(ListNode* head){
+        ListNode*slow=head;
+        ListNode*fast=head;
+
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+
+        while(curr!=NULL){
+            ListNode*nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        return prev;
+    }
     bool isPalindrome(ListNode* head) {
-        stack<int>st;
-        ListNode*temp=head;
-        while(temp!=NULL){
-            st.push(temp->val);
-            temp=temp->next;
+    //    Find the middle of the linked list 
+    //  reverse the section and compare it with the first
+    ListNode*mid=findMid(head);
+    // Now reverse 
+    ListNode*newHead=reverse(mid->next);
+    ListNode*first=head;
+    ListNode*second=newHead;
+
+    while(second!=NULL){
+        if(first->val!=second->val){
+            return false;
         }
-        temp=head;
-        while(temp!=NULL){
-            if(st.top()!=temp->val){
-                return false;
-            }
-            st.pop();
-            temp=temp->next;
-        }
-        return true;
+        first=first->next;
+        second=second->next;
+    }
+    return true;
     }
 };
