875ce6fd11d6198ee8af2384af54aa17a9dc94e1
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-12 14:49:33 +0530

    Time: 4 ms (50.54%), Space: 118.1 MB (64.04%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -7,23 +7,22 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
+
+
+ So my strategy for checking if a linked list is palindrom or not 
+//  is that I will draw analogy with an array in an array I keep two 
+// pointers and keep comparing 
+
+So will do similar here as well , But I cannot traverse backward at the tail 
+But what If I Reverse the second half of the linkedlist that is from slow->next 
+and then eventually start compa
  */
+
 class Solution {
 public:
-    ListNode* findMid(ListNode* head){
-        ListNode*slow=head;
-        ListNode*fast=head;
-
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
-    ListNode* reverse(ListNode* head){
-        ListNode*curr=head;
+    ListNode *reverse(ListNode *head){
         ListNode*prev=NULL;
-
+        ListNode*curr=head;
         while(curr!=NULL){
             ListNode*nextNode=curr->next;
             curr->next=prev;
@@ -32,22 +31,28 @@ public:
         }
         return prev;
     }
-    bool isPalindrome(ListNode* head) {
-    //    Find the middle of the linked list 
-    //  reverse the section and compare it with the first
-    ListNode*mid=findMid(head);
-    // Now reverse 
-    ListNode*newHead=reverse(mid->next);
-    ListNode*first=head;
-    ListNode*second=newHead;
+    ListNode*findMid(ListNode *head){
+        ListNode *slow=head;
+        ListNode *fast=head;
 
-    while(second!=NULL){
-        if(first->val!=second->val){
-            return false;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        first=first->next;
-        second=second->next;
+        return slow;
     }
-    return true;
+    bool isPalindrome(ListNode* head) {
+        ListNode*mid=findMid(head);
+        ListNode*second=reverse(mid->next);
+        ListNode*first=head;
+        
+        while(second!=NULL){
+            if(first->val!=second->val){
+                return false;
+            }
+            first=first->next;
+            second=second->next;
+        }
+        return true;
     }
 };
