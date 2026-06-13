5f26b9c129d4f81c4ac3ba33fd77af36de1c0e62
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-13 14:45:46 +0530

    Time: 58 ms (21.53%), Space: 31.1 MB (20.93%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,3 +1,19 @@
+/*
+
+on using an O(n^3) approach we will get ultimately 
+all the triplets possible but 
+{0,1,-1} and {-1,0,1} as well 
+
+so now these are same but set will not recognise them since
+ordering is different
+
+Now optimising to something better near about o(n^2)
+
+Now practcally we are using third loop because we need the sum 
+But is it there really need of it ??
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -6,12 +22,11 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            // initially if both tend to be equal then move i++
             int j=i+1;
             int k=n-1;
 
             while(j<k){
-                long long sum = 1LL *(nums[i]+nums[j]+nums[k]);
+                long long sum=1LL*(nums[i]+nums[j]+nums[k]);
                 if(sum<0){
                     j++;
                 }
@@ -23,11 +38,11 @@ public:
                     ans.push_back(temp);
                     j++;
                     k--;
-                    while(j<k && nums[j]==nums[j-1])j++;
-                    while(j<k && nums[k]==nums[k+1])k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
-        return ans;
+return ans;  
     }
 };
