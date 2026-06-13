b2111d4ed6128b6756731bc84c5b496752b31697
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-13 08:29:20 +0530

    Time: 3 ms (27.99%), Space: 45 MB (7.62%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,38 @@
+/*
+Array of strings -> words
+weights 26 
+weight[i]=> ith 
+
+weight[0]-> a
+weight[25]-> z
+
+weight of word represent sum of weights
+
+weight %26 =34%26=8-> 
+weight[25]-> a
+
+weight[0]-> z 
+r s t u v w x y z
+*/
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        unordered_map<int,char>mapping;
+        char ch='a';
+        for(int i=25;i>=0;i--){
+            mapping[i]=ch;
+            ch++;
+        }
+        string ans="";
+        for(string s:words){
+            string x=s;
+            int totalW=0;
+            for(int i=0;i<x.length();i++){
+                int a=x[i]-'a';
+                totalW+=weights[a];
+            }
+        ans+=mapping[totalW%26];
+        }
+        return ans;
+    }
+};
