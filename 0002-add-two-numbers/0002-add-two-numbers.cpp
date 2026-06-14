95c331ff2a568e7c4970dde208294b1a81d2efb8
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-14 11:29:09 +0530

    Time: 3 ms (34.72%), Space: 77.1 MB (74.12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -15,27 +15,35 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-          ListNode*t1=l1;
-          ListNode *t2=l2;
-          ListNode *dummy=new ListNode(-1);
-          ListNode *curr=dummy;
-          int carry=0;
-          while(t1!=NULL || t2!=NULL){
-                int sum=carry;
-
-                if(t1) sum+=t1->val;
-                if(t2) sum+=t2->val;
-
-                ListNode *nava=new ListNode(sum%10);
-                curr->next=nava;
-                curr=curr->next;
-                carry=sum/10;
+        // We are definitely needing a new Node 
+        // that is a new Linked List 
+        // During insertion bhi dummy node use hoga
+        ListNode *dummy= new ListNode(-1);
+        ListNode *curr=dummy;
+        // to traverse in the list like we always do similar to a temp
+        // -1-> ()-> ()
+        ListNode *t1=l1;
+        ListNode *t2=l2;
+    // 1 2 3
+    // 2 3 
+        int carry=0;
+        while(t1!=NULL || t2!=NULL){
+            int currSum=carry;
+            if(t1) currSum+=t1->val;
+            if(t2) currSum+=t2->val;
+            ListNode *nextNode=new ListNode(currSum%10);
+            curr->next=nextNode;
+            curr=curr->next;
+            carry=currSum/10;
             if(t1) t1=t1->next;
             if(t2) t2=t2->next;
-          }
-          if(carry!=0){
-            curr->next=new ListNode(carry);
-          }
-          return dummy->next;
+        }
+
+        if(carry){
+            ListNode *some=new ListNode(carry);
+            curr->next=some;
+        }
+
+        return dummy->next;
     }
 };
