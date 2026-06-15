60afed6bbb22289e5607b645bb77118109f75ef2
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-15 11:51:43 +0530

    Time: 57 ms (11.94%), Space: 75.8 MB (19.13%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,22 +10,53 @@
  */
 class Solution {
 public:
+    ListNode *mid(ListNode *head){
+        ListNode *slow=head;
+        ListNode *fast=head->next; // we want middle to stop at 1-> 2-> 3-> 4 that is at 2 
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode *merge(ListNode *l1,ListNode *l2){
+        ListNode *dummy =new ListNode(-1);
+        ListNode *temp=dummy;
+
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val<l2->val){
+                temp->next=l1;
+                temp=temp->next;
+                l1=l1->next;
+            }
+            
+            else {
+                 temp->next=l2;
+                temp=temp->next;
+                l2=l2->next;
+            }
+        }
+        while(l1){
+                temp->next=l1;
+                temp=temp->next;
+                l1=l1->next;
+        }
+        while(l2){
+                temp->next=l2;
+                temp=temp->next;
+                l2=l2->next;
+        }
+        return dummy->next;
+    }
     ListNode* sortList(ListNode* head) {
-        vector<int>arr;
-        ListNode *temp=head;
-
-        while(temp!=NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
-        sort(arr.begin(),arr.end());
-
-        temp=head;
-        for(int i=0;i<arr.size();i++){
-            swap(temp->val,arr[i]);
-            temp=temp->next;
-        }
-        return head;
-
+        if(head==NULL || head->next==NULL) return head;
+        ListNode *middle=mid(head);
+        ListNode *leftHead=head;
+        ListNode *rightHead=middle->next;
+        middle->next=NULL;
+        ListNode* leftHeadNew=sortList(leftHead);
+        ListNode* rightHeadNew=sortList(rightHead);
+        return merge(leftHeadNew,rightHeadNew);
     }
 };
