34ff4d11bad70879358b1eae3b92750b5117d08b
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-18 00:41:22 +0530

    Time: 20 ms (89.66%), Space: 23.9 MB (67.82%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -1,48 +1,45 @@
 class Solution {
 public:
     char processStr(string s, long long k) {
-        string result="";
-        int n=s.size();
-        // In order to evaluate the kth char we don't 
-        // need the entire string because uski need hee nahi 
-        // hume bss kth char chahiye and our job is done 
-// Since directly simulating the construction of result is infeasible,
-        // Instead of trying to determine which character ends up at position k in the final string. We can try to determine 
         long long len=0;
-        for(auto c:s){
-            if(c=='*'){
+
+        for(auto ch:s){
+            if(ch=='#'){
+                len*=2;
+            }
+            else if(ch=='*'){
                 if(len) len--;
             }
-            else if(c=='#'){
-                len *=2;
-            }
-            else if(c=='%'){
+            else if (ch=='%'){
                 continue;
             }
             else{
                 len++;
             }
         }
-        if(k>=len) return '.';
+
+
+        if(k>=len) return '.'; 
+
         for(int i=s.size()-1;i>=0;i--){
             if(s[i]=='*'){
-            len++; 
+                len++;
+            }
+            else if(s[i]=='#'){
+                 len=len/2;
+                 k=(k>=len)?k-len:k;
             }
             else if(s[i]=='%'){
                 k=len-k-1;
             }
-            else if(s[i]=='#'){
-                if(k>(len-1)/2){
-                    k=k-len/2;
-                }
-                len=(len+1)/2;
-            }
-            else{
-                if(k+1==len) return s[i];
-                else len--;
+            else {
+                len--;
             }
 
+            if(k==len){
+                return s[i];
+            }
         }
-        return '.';
+    return s[k];
     }
 };
