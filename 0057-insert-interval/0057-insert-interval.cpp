60d62379536c47b679d38b8dc53d607a8ec9fd2a
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-18 00:04:05 +0530

    Time: 0 ms (100%), Space: 21.6 MB (94.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>>result;
         int n=intervals.size();
         int i=0;
-            while(i<n&&intervals[i][1]<newInterval[0]){
-                result.push_back(intervals[i]);
+        vector<vector<int>>ans;
+        while(i<n&& intervals[i][1]<newInterval[0]){
+            
+                ans.push_back(intervals[i]);
                 i++;
-            }
-            while(i<n&&intervals[i][0]<=newInterval[1]){
-                newInterval[0]=min(newInterval[0],intervals[i][0]);
-                newInterval[1]=max(newInterval[1],intervals[i][1]);
+        }
+         while(i<n &&intervals[i][0]<=newInterval[1]){
+                newInterval[1]=max(intervals[i][1],newInterval[1]);
+                newInterval[0]=min(intervals[i][0],newInterval[0]);
                 i++;
-            }
-            result.push_back(newInterval);
-            while(i<n){
-                result.push_back(intervals[i]);
+        }
+        ans.push_back(newInterval);
+        while(i<n){
+            ans.push_back(intervals[i]);
                 i++;
-            }
-        return result;
+        }
+        
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
