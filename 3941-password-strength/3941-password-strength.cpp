d61030a9c782d9025ce2749526b6811d617a2cb2
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-18 20:21:24 +0530

    Time: 28 ms (24.71%), Space: 17.8 MB (21.86%) - LeetHub

--- 3941-password-strength/3941-password-strength.cpp
+++ 3941-password-strength/3941-password-strength.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    bool isDigit(char c){
-        if(c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'
-        ||c=='9'||c=='0' ){
-            return true;
-        }
-        return false;
-    }
     int passwordStrength(string password) {
         int ans=0;
         set<char>st;
@@ -18,7 +11,7 @@ public:
         for(auto it:st){
             if(islower(it)) ans+=1;
            else if(isupper(it)) ans+=2;
-           else if(isDigit(it)) ans+=3;
+           else if(isalnum(it)) ans+=3;
            else ans+=5;
         }
         return ans;
