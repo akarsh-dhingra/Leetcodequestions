58becdc04e9cd6ab6d30a5764958ea5707765064
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-22 10:44:10 +0530

    Time: 59 ms (5.37%), Space: 17.6 MB (47.56%) - LeetHub

--- 3941-password-strength/3941-password-strength.cpp
+++ 3941-password-strength/3941-password-strength.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int passwordStrength(string password) {
-        int ans=0;
-        set<char>st;
 
-        for(char ch:password){
-            st.insert(ch);
-            
+    int passwordStrength(string password) {
+        int score=0;
+        // at most once 
+        // set 
+        set<char>st;
+        for(int i=0;i<password.size();i++){
+            if(islower(password[i])&& st.find(password[i])==st.end()){
+                score+=1;
+                }
+            else if(isupper(password[i]) && st.find(password[i])==st.end() )score+=2;
+            else if(isalnum(password[i]) && st.find(password[i])==st.end()) score+=3;
+            else if(st.find(password[i])==st.end()) score+=5;
+            st.insert(password[i]);
         }
-        for(auto it:st){
-            if(islower(it)) ans+=1;
-           else if(isupper(it)) ans+=2;
-           else if(isalnum(it)) ans+=3;
-           else ans+=5;
-        }
-        return ans;
+    return score;
     }
 };
