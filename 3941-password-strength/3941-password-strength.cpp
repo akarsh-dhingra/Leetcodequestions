1f7e3ea67a6116f048abb2b45764a20f6b2e26fd
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-22 10:46:53 +0530

    Time: 35 ms (13.4%), Space: 17.7 MB (21.81%) - LeetHub

--- 3941-password-strength/3941-password-strength.cpp
+++ 3941-password-strength/3941-password-strength.cpp
@@ -7,12 +7,15 @@ public:
         // set 
         set<char>st;
         for(int i=0;i<password.size();i++){
-            if(islower(password[i])&& st.find(password[i])==st.end()){
-                score+=1;
-                }
-            else if(isupper(password[i]) && st.find(password[i])==st.end() )score+=2;
-            else if(isalnum(password[i]) && st.find(password[i])==st.end()) score+=3;
-            else if(st.find(password[i])==st.end()) score+=5;
+            if(st.find(password[i])!=st.end()){
+                continue;
+            }
+            else{
+            if(islower(password[i]))score+=1;
+            else if(isupper(password[i]))score+=2;
+            else if(isalnum(password[i])) score+=3;
+            else score+=5;
+            }
             st.insert(password[i]);
         }
     return score;
