e0c43544d6ae6248cf4ff33f27a0b08f392417a5
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-25 10:06:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (35.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,7 +7,7 @@ class Solution {
         return oneStep+twoStep;
      }
      int solveUsingMem(vector<int>&dp,int n){
-       if(n==0||n==1) return 1;
+       if(n==0||n==1) return dp[n]=1;
        if(dp[n]!=-1) return dp[n];
         int oneStep=solveUsingMem(dp,n-1);
         int twoStep=solveUsingMem(dp,n-2);
@@ -22,8 +22,18 @@ class Solution {
         }
         return dp[n];
      }
+     int solveUsingSpaceOpt(int n){
+        int prev1=1;
+        int prev2=1;
+        int curr=0;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+
+        }
+        return curr;
+     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return solveUsingTab(n);
+        return solveUsingMem(dp,n);
     }
 };
