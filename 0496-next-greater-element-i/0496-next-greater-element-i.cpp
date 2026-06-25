f46f1321c2d406594b6ab5a3f48930a9e873fc17
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-25 21:27:40 +0530

    Time: 2 ms (46.96%), Space: 12.9 MB (15.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         map<int,int>ngemap;
+        int n=nums2.size();
+        vector<int>ans;
         stack<int>st;
-        for(int i=nums2.size()-1;i>=0;i--){
-            while(!st.empty()&&st.top()<=nums2[i]){
-                st.pop(); // I am not finding any next greaeter element I start popping outapplicable for less and equal cases 
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&& st.top()<=nums2[i]){
+                st.pop();
             }
             if(st.empty()){
-                ngemap[nums2[i]]=-1; // if initially stack is empty from the back push -1l
+                ngemap[nums2[i]]=-1;
             }
             else{
-                ngemap[nums2[i]]=st.top(); // 
+                ngemap[nums2[i]]=st.top();
             }
             st.push(nums2[i]);
         }
-        vector<int>ans;
+
         for(auto it:nums1){
             ans.push_back(ngemap[it]);
         }
-        return ans;
+
+      return ans;
     }
 };
