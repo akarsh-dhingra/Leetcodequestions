a13794c0a68f8014b0328335577105e989d0df67
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-27 16:45:00 +0530

    Time: 21 ms (64.38%), Space: 139.2 MB (66.71%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int>dq;
-        vector<int>result;
-        for(int i=0;i<nums.size();i++){
-            // removal of elements outside the curr window.
-            if(!dq.empty()&&dq.front()<=i-k){
+
+        vector<int>res;
+        int n=nums.size();
+        for(int i=0;i<k;i++){
+            while(dq.size()>0&&nums[dq.back()]<=nums[i]){
+                    dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        for(int i=k;i<n;i++){
+            // push back answer of older window
+            res.push_back(nums[dq.front()]);
+
+            while(dq.size()>0&& dq.front()<=i-k){
                 dq.pop_front();
             }
-            //if added element is greater than the back element disturbin monotonicity
-            while(!dq.empty()&&nums[dq.back()]<=nums[i]){
-                dq.pop_back();
+            // remove the smaller values from end
+            while(dq.size()>0&&nums[dq.back()]<=nums[i]){
+                    dq.pop_back();
             }
-            dq.push_back(i); // insert from the back i.e. right side.
-            if(i>=k-1){
-                result.push_back(nums[dq.front()]);
-            }
+            dq.push_back(i);
         }
-        return result;
+        res.push_back(nums[dq.front()]);
+        return res;
     }
 };
