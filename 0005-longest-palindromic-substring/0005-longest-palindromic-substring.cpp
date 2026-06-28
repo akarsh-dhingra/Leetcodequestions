6528e508775f0f9a1f840f6659a81be307a41def
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-28 23:53:07 +0530

    Time: 114 ms (24.93%), Space: 26 MB (30.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-bool ispalindrome(string&t, int low,int high){
-    while(low<=high){
-        if(t[low]!=t[high]){
-            return false;
-        }
-        low++;
-        high--;
-    }
-    return true;
-}
     string longestPalindrome(string s) {
-        string ans="";
-        int maxlen=INT_MIN;
-        for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                if(ispalindrome(s,i,j)){
-                    string subst=s.substr(i,j-i+1); // using this line before if will increase no. of innecessary operations due to which TLE occurs.
-                    ans=subst.size()>ans.size()?subst:ans;
+        int n=s.length();
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        int start=0;
+        int maxLen=1;
+        // for 1 length vaale substring 
+
+        for(int i=0;i<n;i++){
+            dp[i][i]=true;
+        }
+
+        // for 2 length vaale substring 
+
+        for(int i=0;i<n-1;i++){
+            if(s[i]==s[i+1]){
+                dp[i][i+1]=true;
+                // Logic behind this is agr 3 vaalo mai kuch nahi aata 
+                // toh this will be the answer 
+                start=i;
+                maxLen=2;
+            }
+        }
+
+        for(int len=3;len<=n;len++){
+            
+            for(int i=0;i<=n-len;i++){
+
+                int j=i+len-1;
+
+                if(s[i]==s[j] && dp[i+1][j-1]==true){
+                    dp[i][j]=true;
+                    if(len>maxLen){
+                        maxLen=len;
+                        start=i;
+                    }
                 }
             }
         }
-        return ans;
+    return s.substr(start,maxLen);
     }
 };
