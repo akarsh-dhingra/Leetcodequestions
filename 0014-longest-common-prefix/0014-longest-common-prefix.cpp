6911c3e2604aaa1e3e442d4bbf088f602dceacad
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-29 20:03:40 +0530

    Time: 0 ms (100%), Space: 12.1 MB (14.22%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-     // sbse pehle curr_ch i.e joh compare krna hai voh =0 
-     string ans="";
-     int indx=0; // indx will help in iterating
-// jab bhi nah pta ho ki kya kha break kaise ak inf loop
- while(true){
-char curr_ch=0;
-for(auto str:strs){
-    if(indx>=str.length()){
-        curr_ch=0;
-        break;
-    }
-    if(curr_ch==0){
-        curr_ch=str[indx];
-    }
-    else if(curr_ch!=str[indx]){
-        curr_ch=0; // kyunki unequal aaya hai toh definitely yeh answer ka part nahi hoga
-        break;
-    }
-}
-if(curr_ch==0){
-    break;
-}
-    ans.push_back(curr_ch);
-    indx++;
-     }
-return ans;
+        sort(strs.begin(),strs.end());
+        //o(nlogn)
+        int n=strs.size();
+        string a=strs[0];
+        string b=strs[n-1];
+        int x=a.size();
+        int y=b.size();
+        int i=0;
+        int j=0;
+        while(i<x && j<y){
+            if(a[i]!=b[j]){
+                return a.substr(0,j);
+            }
+            i++;
+            j++;
+        }
+        return a;
     }
 };
