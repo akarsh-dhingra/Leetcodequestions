071e241df6f67d38ee8e829fff41777201cfe9a1
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-29 10:26:56 +0530

    Time: 20 ms (5.13%), Space: 49.3 MB (5.26%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        // the sky is blue
-        // blue is sky the 
-        // eulb si yks eht
-
+        // reverse the entire string 
         reverse(s.begin(),s.end());
         string ans="";
+        if(s==" ")return "";
         int n=s.length();
         for(int i=0;i<s.length();i++){
-            // We will extract individual words
-              string word="";
-              while(i<n && s[i]!=' '){
-                    word+=s[i];
-                    i++;
-              }
-              reverse(word.begin(),word.end());
-              if(word.length()>0) ans+=" "+word;
-        }
+           string temp="";
+            while(i<n && s[i]!=' '){
+                temp+=s[i];
+                i++;
+            }
+            if(temp.size()>0){
+                reverse(temp.begin(),temp.end());
+                ans=ans+" "+temp;
+            }
+           }
         return ans.substr(1);
     }   
 };
