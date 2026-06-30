3f096a30907fc2a765213ec7eb45dd78b15ae715
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-06-30 19:38:31 +0530

    Time: 3 ms (54.18%), Space: 10.9 MB (70.33%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char, int> freq;
-        for(char c:s){
-            freq[c]++;
-        }
-        // frequency stored!
-// data copied from map into vector
-        vector<pair<char,int>>freqVec(freq.begin(),freq.end());
-        // sorted as per frequecy
-     sort(freqVec.begin(), freqVec.end(), [](auto &a, auto &b) {
-        return a.second > b.second;
-    });
-    string res;
-    for(auto it:freqVec){
-        res.append(it.second,it.first);
-    }
+        unordered_map<char,int>mpp;
+        for(auto it:s){
+            mpp[it]++;
+        }            
+        vector<pair<char,int>>v(mpp.begin(),mpp.end());
+        sort(v.begin(),v.end(),[](const auto &a,const auto &b){
+            return a.second>b.second;
+        });
+        string ans="";
 
-        // int freqmax=INT_MIN;
-        // for(auto it:mpp){
-        //     freqmax=max(freqmax,it.second);
-        // }
-        // vector<int>t(mpp.size());
-        // for(auto it:t){
-        //     t.push_back()
-        // }
-        return res;
+        for(auto it:v){
+            for(int i=0;i<it.second;i++)
+                ans+=it.first;
+        }
+        return ans;
+    
     }
 };
