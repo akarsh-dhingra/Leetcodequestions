6b56b33d0fa3f879ddbb90e29f000ab96a493096
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-01 11:22:48 +0530

    Time: 102 ms (27.69%), Space: 25.9 MB (32.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.length();
+            int n=s.size();
+            int maxLen=1;
+            int start=0;
         vector<vector<bool>>dp(n,vector<bool>(n,false));
-        int start=0;
-        int maxLen=1;
-        // for 1 length vaale substring 
 
         for(int i=0;i<n;i++){
             dp[i][i]=true;
         }
 
-        // for 2 length vaale substring 
-
         for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                dp[i][i+1]=true;
-                // Logic behind this is agr 3 vaalo mai kuch nahi aata 
-                // toh this will be the answer 
-                start=i;
-                maxLen=2;
+            if(s[i]==s[i+1]) {
+            dp[i][i+1]=true;
+            start=i;
+            maxLen=2;
             }
         }
 
         for(int len=3;len<=n;len++){
-            
+        
             for(int i=0;i<=n-len;i++){
 
                 int j=i+len-1;
-
-                if(s[i]==s[j] && dp[i+1][j-1]==true){
+                if(s[i]==s[j]&& dp[i+1][j-1]){
                     dp[i][j]=true;
-                    if(len>maxLen){
-                        maxLen=len;
+                    if(j-i+1>maxLen){
+                        maxLen=j-i+1;
                         start=i;
                     }
                 }
