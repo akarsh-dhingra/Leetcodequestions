7dcf0fd2225fec09273c4a3721ecbf805cabc588
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-02 21:15:43 +0530

    Time: 0 ms (100%), Space: 9.2 MB (45.46%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,9 +1,47 @@
+#define ll long long int
+#define MOD 1000000007
 class Solution {
+    // Radix is the base of the number system in 
+    // our case it is 26
 public:
-    int strStr(string haystack, string needle) {
-        if(haystack.find(needle)!=-1){
-            return haystack.find(needle);
+    ll hashValue(string string,ll radix,ll m){
+        ll ans=0,factor=1;
+        for(ll i=m-1;i>=0;i--){
+            ans+=((string[i]-'a')*factor)%MOD;
+            factor=(factor*radix)%MOD;
         }
-        else return -1;
+        return ans%MOD;
+    }
+    int strStr(string haystack, string needle) {
+        ll n=haystack.size();
+        ll m=needle.size();
+
+        if(n<m) return -1;
+
+        ll radix=26;
+        ll MAX_WEIGHT=1;
+
+        for(ll i=1;i<=m;i++){
+            MAX_WEIGHT=(MAX_WEIGHT*radix)%MOD;
+            // (26)^m
+        }
+        
+        ll hashNeedle=hashValue(needle,radix,m);
+        ll hashhay=0;
+        for(ll i=0;i<=n-m;i++){
+            if(i==0){
+                hashhay=hashValue(haystack,radix,m);
+            }
+            else{
+        hashhay=((hashhay*radix)%MOD-((haystack[i-1]-'a')*MAX_WEIGHT)%MOD+(haystack[i+m-1]-'a')+MOD)%MOD;
+            }
+            if(hashNeedle==hashhay){
+                for(ll j=0;j<m;j++){
+                    if(needle[j]!=haystack[j+i]) break;
+                    if(j==m-1) return i;
+                }
+            }
+        }
+        return -1;
     }
 };
