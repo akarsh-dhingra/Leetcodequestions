1726b4861e37d3de6d118b66c50ba334ff3014b2
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-04 16:18:36 +0530

    Time: 453 ms (9.57%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT author_id AS id
-FROM Views
-WHERE author_id=viewer_id
-ORDER BY author_id ASC
+SELECT distinct(v.author_id) as id
+FROM VIEWS as v
+where v.author_id=v.viewer_id
+ORDER BY v.author_id ASC
