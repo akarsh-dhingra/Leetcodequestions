8bd2545943a37ed64e1f8bbb1df3277f8c65af31
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-04 11:22:42 +0530

    Time: 0 ms (100%), Space: 21.6 MB (79.01%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -7,9 +7,11 @@ public:
         if(a>0) st.push_back(a);
         else{
             while(!st.empty()&& st.back()>0 &&st.back()<abs(a)){
+                // >0 because We know that <0
                 st.pop_back();
             }
             if(!st.empty()&&st.back()==abs(a))st.pop_back();
+            // [8,-8]
             else if(st.empty()||st.back()<0){
                 st.push_back(a);
             }
