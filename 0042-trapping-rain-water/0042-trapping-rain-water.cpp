19fcc855fa23054da6cbe93e67dd5d895d9eccfa
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-05 15:11:40 +0530

    Time: 0 ms (100%), Space: 26.1 MB (57.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-
     int trap(vector<int>& height) {
         int n=height.size();
-        int total=0;
-        vector<int>prefix(n,0);
-        vector<int>suffix(n,0);
-        suffix[n-1]=height[n-1];
-        prefix[0]=height[0];
 
-        for(int i=1;i<n;i++){
-            prefix[i]=max(prefix[i-1],height[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],height[i]);
-        }
-        for(int i=0;i<n;i++){
-            int leftMax=prefix[i];
-            int rightMax=suffix[i];
-            if(height[i]<leftMax && rightMax>height[i]){
-                total+=(min(leftMax,rightMax)-height[i]);
+        int left=0;
+        int right=n-1;
+
+        int maxLeft = 0;
+        int maxRight = 0;
+
+        int totalWater=0;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=maxLeft){
+                    maxLeft=height[left];
+                }
+                else{
+                    totalWater+=maxLeft-height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>=maxRight){
+                    maxRight=height[right];
+                }
+                else{
+                    totalWater+=maxRight-height[right];
+                }
+                right--;
             }
         }
-        return total;
+    return totalWater;
     }
 };
