c59bb75515769fbad53b5c97dc80e3810115f977
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-05 13:39:12 +0530

    Time: 0 ms (100%), Space: 13.1 MB (15.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>ngemap;
-        int n=nums2.size();
-        vector<int>ans;
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty()&& st.top()<=nums2[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                ngemap[nums2[i]]=-1;
-            }
-            else{
-                ngemap[nums2[i]]=st.top();
-            }
-            st.push(nums2[i]);
-        }
 
-        for(auto it:nums1){
-            ans.push_back(ngemap[it]);
-        }
+    // nge ka code likhte hai for nums2 
+    // nums1 ka liye fir iterate krkr nikal lenge
+    int n=nums2.size();
 
-      return ans;
+    stack<int>st;
+    map<int,int>nge;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty()&& st.top()<=nums2[i]){
+            st.pop();
+        }
+        nge[nums2[i]]=st.empty()?-1:st.top();
+        st.push(nums2[i]);
+    }
+
+    vector<int>ans;
+    for(int i=0;i<nums1.size();i++){
+        if(nge.find(nums1[i])!=nge.end()){
+            ans.push_back(nge[nums1[i]]);
+        }
+    }
+    return ans;
+
     }
 };
