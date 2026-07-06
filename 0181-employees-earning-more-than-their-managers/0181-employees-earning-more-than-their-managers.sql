13db21cfa3f13d5e9180b998d29f45c2bbcdddfb
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-06 10:43:00 +0530

    Time: 222 ms (92.47%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,8 +1,8 @@
 -- Write your PostgreSQL query statement below
 -- Find Employees who earn more than their managers 
 
-SELECT m.name as Employee
-FROM Employee as m
-JOIN Employee as e
-ON m.managerId=e.id
-WHERE m.salary>e.salary
+SELECT e.name as employee
+FROM Employee as e
+JOIN Employee as m
+ON e.managerId=m.id
+WHERE e.salary>m.salary
