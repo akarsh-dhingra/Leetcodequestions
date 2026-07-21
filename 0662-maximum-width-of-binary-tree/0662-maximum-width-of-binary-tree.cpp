49458d61230e459bf558d337d1db79389ca7999d
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-21 16:53:04 +0530

    Time: 0 ms (100%), Space: 19.7 MB (75.98%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,28 +12,27 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
+        // Ultimately we've to find distance between two nodes
+        // that are at the extreme levels 
         if(root==NULL) return 0;
         queue<pair<TreeNode*,long long>>que;
         que.push({root,0});
         int ans=0;
         while(!que.empty()){
+            // hum har level ka saare mai sa MinIndx nikal lenge usse fir aage jaane vaale Indexes 
+            // ko update krte jayenge 
             int size=que.size();
-            TreeNode* currNode=que.front().first;
             long long mIndx=que.front().second;
             int first,last;
             for(int i=0;i<size;i++){
                 long long curr_id=que.front().second-mIndx;
-                TreeNode *node=que.front().first;
+                TreeNode*node=que.front().first;
                 que.pop();
                 if(i==0) first=curr_id;
                 if(i==size-1) last=curr_id;
-                if(node->left){
-                    que.push({node->left,curr_id*2+1});
-                }
-                if(node->right){
-                    que.push({node->right,curr_id*2+2});
-                }
-          }
+                if(node->left) que.push({node->left,2*curr_id+1});
+                if(node->right) que.push({node->right,2*curr_id+2});
+            }
         ans=max(ans,last-first+1);
         }
         return ans;
