147314be6b85fc9277172e9adaae48a0d53a1d54
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-21 21:28:19 +0530

    Time: 54 ms (17.75%), Space: 88.8 MB (11.22%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -10,7 +10,7 @@ public:
         map<int,int>mpp;
         while(r<n){
             mpp[fruits[r]]++;
-            while(mpp.size()>2){
+            if(mpp.size()>2){
                 mpp[fruits[l]]--;
                 if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
                 l++;
