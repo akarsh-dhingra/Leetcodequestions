ab787395c5e786214611ae5c972712fb68f83464
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-22 10:36:31 +0530

    Time: 7 ms (61.15%), Space: 11.5 MB (43.6%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // generate all the substrings and compare their freuqnncies 
-        // against t 
-        int n=s.length();
-        long long m=t.size();
-        long long sIndx=-1;
+        unordered_map<char,int>mpp;
+        for(auto ch:t){
+            mpp[ch]++;
+        }
+        int n=s.size();
+        
+        int sIndx=-1;
         long long minLen=INT_MAX;
-          vector<int>hash(256,0);
-             for(auto ch:t){
-                hash[ch]++;
-            }
         int l=0;
         int r=0;
-        int cnt=0;
-        while(r<s.size()){
-            if(hash[s[r]]>0) cnt=cnt+1;
-            hash[s[r]]--;
-
-            while(cnt==m){
-                if(r-l+1<minLen){
-                    minLen=r-l+1;
+        long long cnt=0;
+        while(r<n){
+            if(mpp[s[r]]>0) cnt+=1;
+            mpp[s[r]]--;
+            while(cnt==t.size()){
+                if(minLen>r-l+1){
                     sIndx=l;
+                    minLen=r-l+1;
                 }
-                hash[s[l]]++;
-                if(hash[s[l]]>0) cnt-=1;
+                mpp[s[l]]++; // coz reInserting that's why ++
+                if(mpp[s[l]]>0) cnt-=1;
                 l++;
             }
-        r++;
+            r++;
         }
-        if(sIndx == -1)return "";
+        if(sIndx==-1) return "";
         return s.substr(sIndx,minLen);
     }
 };
