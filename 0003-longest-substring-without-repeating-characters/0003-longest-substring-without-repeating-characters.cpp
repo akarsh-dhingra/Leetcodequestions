327931b898a2fd26b45bada3d4befbb5572a954d
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-22 14:56:32 +0530

    Time: 15 ms (36.05%), Space: 12.5 MB (36.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,13 +4,18 @@ public:
             int n=s.length();
             int maxlen=INT_MIN;
             if(n==0) return 0;
-            for(int i=0;i<n;i++){
-                unordered_map<char,int>mpp;
-                for(int j=i;j<n;j++){
-                    if(mpp.find(s[j])!=mpp.end()) break;
-                    mpp[s[j]]++;
-                    maxlen=max(maxlen,j-i+1);
+            int l=0;
+            int r=0;
+            unordered_map<char,int>mpp;
+            while(r<n){
+                mpp[s[r]]++;
+                while(mpp[s[r]]>1){
+                       mpp[s[l]]--;
+                       if(mpp[s[l]]==0) mpp.erase(s[l]);
+                       l++;
                 }
+                maxlen=max(maxlen,r-l+1);
+                r++;
             }
         return maxlen;
     }
