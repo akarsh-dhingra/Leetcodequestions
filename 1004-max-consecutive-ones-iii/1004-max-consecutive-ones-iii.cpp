b2a638b6be43e50c5a01c87c65e6b52e2b3fef63
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-23 08:10:11 +0530

    Time: 0 ms (100%), Space: 69.6 MB (31.06%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
+        // maximum no. of consecutive ones is you can flip atmost k 0's
+        // nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+
+    // maximize consectuvie ones 
+        // Ultimately question becomes to find the longest substring with at most k 0's 
+
         int n=nums.size();
+        int maxlen=INT_MIN;
+
         int l=0;
         int r=0;
         int cnt=0;
-        int zeros=0;
-        int maxLen=0;
         while(r<n){
-            if(nums[r]==0){
-                zeros++;
-            }
-            if(zeros>k){
-                if(nums[l]==0)zeros--;
+            if(nums[r]==0) cnt++;
+            while(cnt>k){
+                if(nums[l]==0) cnt-=1;
                 l++;
             }
-            if(zeros<=k){
-            int len=r-l+1;
-            maxLen=max(maxLen,len);
-            }
+            maxlen=max(maxlen,r-l+1);
             r++;
         }
-        return maxLen;
+        if(maxlen==INT_MIN) return 0;
+        return maxlen;
     }
 };
