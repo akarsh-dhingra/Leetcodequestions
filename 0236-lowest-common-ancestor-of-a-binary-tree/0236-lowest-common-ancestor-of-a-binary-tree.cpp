f005dfe17efbf189799dc16ac8afb33589fccb8f
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-23 15:01:17 +0530

    Time: 67 ms (31.84%), Space: 43.8 MB (64.82%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,26 +10,15 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-      // NULL
-      if(root==NULL) return NULL;
-      // it is used for cases 2-6 or 
-      // it is used for finding the root pa and q whose lca we have to find later on jinhe backtrack we'll find lca
-      if(root==p||root==q) return root; 
-      // intution behind leftNode and RightNode is ki two cases can exist both lie in the same subtree or diff
-      TreeNode*leftAns=lowestCommonAncestor(root->left,p,q);
-      TreeNode*rightAns=lowestCommonAncestor(root->right,p,q);
-
-      if(leftAns!=NULL&&rightAns!=NULL){
-        return root;
-      }
-     else if(leftAns!=NULL&&rightAns==NULL){
-        return leftAns;
-      }
-      else if(leftAns==NULL&&rightAns!=NULL){
-        return rightAns;
-      }
-      else{
-        return NULL;
-      }
+        if(root==NULL) return NULL;
+        if(root==p||root==q) return root;
+        TreeNode*leftAns=lowestCommonAncestor(root->left,p,q);
+        TreeNode*rightAns=lowestCommonAncestor(root->right,p,q);
+        
+        if(leftAns!=NULL && rightAns!=NULL) return root;
+        else if(leftAns!=NULL && rightAns==NULL) return leftAns;
+        else if(leftAns==NULL && rightAns!=NULL) return rightAns;
+        else return NULL;
+    
     }
 };
