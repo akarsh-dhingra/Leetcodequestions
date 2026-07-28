c9a52744961ad6e37b70703d759b272d1996c4cd
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-28 21:51:18 +0530

    Time: 56 ms (52.28%), Space: 55.5 MB (21.54%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -5,10 +5,11 @@
 var compose = function(functions) {
 
     return function(x) {
-        return functions.reduceRight((value,fn)=>{
-            return fn(value);
-        },
-        x);
+        let value=x;
+        for(let i=functions.length-1;i>=0;i--){
+            value=functions[i](value);
+        }
+        return value;
     }
 };
 
