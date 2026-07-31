9d35820e393378e5d618349ba7ff31bd731fc551
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-31 17:13:36 +0530

    Time: 316 ms (12.63%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,10 @@
-SELECT t1.id
-FROM Weather as t1
-JOIN Weather as t2
-ON t1.recordDate=t2.recordDate+ INTERVAL '1 days'
-WHERE t1.temperature>t2.temperature
+-- Temperature higher than the previous Days 
+
+SELECT t.id
+FROM Weather as t
+JOIN Weather as y
+ON t.recordDate=y.recordDate+1
+WHERE t.temperature>y.temperature
+-- Today's DATE=Yesterday's Date+1
+-- Yesterday's Date=Today's DATE-1
+
