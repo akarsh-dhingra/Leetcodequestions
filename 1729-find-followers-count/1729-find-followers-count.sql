7c5668d8274a83e8da354002a2992e85a766d29f
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-31 20:57:51 +0530

    Time: 688 ms (16.7%), Space: 0B (100%) - LeetHub

--- 1729-find-followers-count/1729-find-followers-count.sql
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -1,5 +1,5 @@
 -- No. of followers for each user 
-SELECT user_id,COUNT(follower_id) AS followers_count
+SELECT user_id,COUNT(follower_id) as followers_count
 FROM Followers
 GROUP BY user_id
 ORDER BY user_id ASC
