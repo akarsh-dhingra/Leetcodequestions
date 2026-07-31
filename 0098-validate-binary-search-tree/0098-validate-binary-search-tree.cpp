1132eec0625a9974daec08d9630a8976d0ee4c09
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-07-31 11:28:29 +0530

    Time: 0 ms (100%), Space: 22 MB (45.5%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,49 +11,19 @@
  */
 class Solution {
 public:
-bool validate(TreeNode* &root,long minval,long maxval){
-     if(root==NULL){
-        return true;
-     }
-     if(root->val<=minval || root->val>=maxval){
-        return false;
-     }
-     return validate(root->right,root->val,maxval)&&validate    (root->left,minval,root->val);
-}
+    bool checkKrLe(TreeNode* root, long long  maxi,long long mini){
+        if(root==NULL){
+            return true;
+        }
+        if(root->val>mini && root->val<maxi){
+            bool left=checkKrLe(root->left,root->val,mini);
+            bool right=checkKrLe(root->right,maxi,root->val);
+            return left && right;
+        }
+        else 
+            return false;
+    }
     bool isValidBST(TreeNode* root) {
-      return validate(root,LONG_MIN,LONG_MAX);
-        
-        // vector<int>given;
-        // vector<int>temp=inorderTraversal(root);
-        // queue<TreeNode*>q;
-        // q.push(root);
-        // while(!q.empty()){
-        //     TreeNode*currele=q.front();
-        //     given.push_back(currele->val);
-        //     if(root->left){
-        //         q.push(root->left);
-        //     }
-        //     else{
-        //         q.push(root->right);
-        //     }
-        // }
-        // sort(given.begin(),given.end());
-        // if(temp==given){
-        //     return true;
-        // }
-        // return false;
-        // // if inorder traversal sa sorted aa rhi hai toh we can comment it is a bst else we cannot
-    //     queue<TreeNode*>q;
-    //     q.push(root);
-    //     while(!q.empty()){
-    //         TreeNode*currele=q.front();
-    //         if(currele->left){
-    //             q.push(currele->left);
-    //         }
-    //         else{
-    //             q.push(currele->right);
-    //         }
-    //     }
-    //     while(!q.empty())
+        return checkKrLe(root,LLONG_MAX,LLONG_MIN);
     }
 };
