d4200436c86de0d24276ced10e7ecbbe1eab2f8d
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-01 17:42:20 +0530

    Time: 3 ms (31.61%), Space: 10.4 MB (68.42%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-void f(int indx,vector<vector<int>>&result,vector<int>&curr,vector<int>& nums){
-result.push_back(curr);
-for(int i=indx;i<nums.size();i++){
-    if(i!=indx&&nums[i]==nums[i-1])continue;
-    curr.push_back(nums[i]);
-    f(i+1,result,curr,nums);
-    curr.pop_back();
-}
-return;
-}
+void solve(vector<vector<int>>&ans,vector<int>&temp,vector<int>& nums,int indx,int n){
+    // Agr mai ak baar ak element ko exclude kr chuka hu toh aage ka saare same elements 
+    // kabhi incluce nahiu ho skte !!!
+    if(indx==n){
+        ans.push_back(temp);
+        return;
+    }
+    temp.push_back(nums[indx]);
+    solve(ans,temp,nums,indx+1,n);
+    temp.pop_back();
+    while(indx+1<n && nums[indx]==nums[indx+1]) indx++;
+    solve(ans,temp,nums,indx+1,n);
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>>result;
-        vector<int>curr;
-        f(0,result,curr,nums);
-        return result;
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int indx=0;
+        int n=nums.size();
+        solve(ans,temp,nums,0,n);
+        return ans;
     }
 };
