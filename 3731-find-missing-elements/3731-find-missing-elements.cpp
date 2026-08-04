e7ef230afc5e35604a43203ae6c123aa9e3fef82
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-04 08:34:34 +0530

    Time: 4 ms (35.49%), Space: 32.4 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    bool findOut(vector<int>&nums,int target){
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
     vector<int> findMissingElements(vector<int>& nums) {
 auto minii = min_element(nums.begin(), nums.end());
 auto maxii = max_element(nums.begin(), nums.end());
@@ -7,10 +24,12 @@ auto maxii = max_element(nums.begin(), nums.end());
 // To access the values, dereference them with '*'
 int mini =*minii;
 int maxi=*maxii;
+sort(nums.begin(),nums.end());
         vector<int>ans;
         for(int i=mini;i<=maxi;i++){
-            auto it=find(nums.begin(),nums.end(),i);  // o(n)
-            if(it==nums.end()){
+            // I have to answer here is x present in my array that too faster than o(n)
+            // 
+            if(!findOut(nums,i)){
                 ans.push_back(i);
             }
         }
