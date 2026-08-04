0602efdeb72373319665040d3db8f5a33d0aea80
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-04 08:39:22 +0530

    Time: 3 ms (49.33%), Space: 33.7 MB (17.56%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    bool findOut(vector<int>&nums,int target){
-        int low=0;
-        int high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                return true;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return false;
-    }
     vector<int> findMissingElements(vector<int>& nums) {
 auto minii = min_element(nums.begin(), nums.end());
 auto maxii = max_element(nums.begin(), nums.end());
@@ -25,11 +8,17 @@ auto maxii = max_element(nums.begin(), nums.end());
 int mini =*minii;
 int maxi=*maxii;
 sort(nums.begin(),nums.end());
+unordered_map<int,int>mpp;
+int x=0;
+for(auto it:nums){
+    mpp[it]=x;
+    x++;
+}
         vector<int>ans;
         for(int i=mini;i<=maxi;i++){
             // I have to answer here is x present in my array that too faster than o(n)
             // 
-            if(!findOut(nums,i)){
+            if(mpp.find(i)==mpp.end()){
                 ans.push_back(i);
             }
         }
