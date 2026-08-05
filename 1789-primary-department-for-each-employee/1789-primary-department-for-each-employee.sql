4d31cf9ab7d73946dbda4bca346c63680a9c87a3
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-05 21:45:28 +0530

    Time: 861 ms (6.29%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,14 +1,8 @@
--- that when an employee belongs to only one department, their primary column is 'N'.
-SELECT
-    e.employee_id,
-    CASE
-        WHEN COUNT(*) = 1 THEN MAX(department_id)
-        ELSE MAX(
-            CASE
-                WHEN primary_flag = 'Y'
-                THEN department_id
-            END
-        )
-    END AS department_id
-FROM Employee AS e
-GROUP BY e.employee_id;
+SELECT employee_id,department_id
+FROM EMPLOYEE
+WHERE primary_flag='Y' OR employee_id IN(
+    SELECT employee_id
+    FROM EMPLOYEE
+    GROUP BY employee_id
+    HAVING COUNT(employee_id)=1
+    )
