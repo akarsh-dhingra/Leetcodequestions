45971ad9bb41cbdf439419ead0629ede8638d9df
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-05 12:19:14 +0530

    Time: 54 ms (29.84%), Space: 31.2 MB (14.64%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,48 +1,36 @@
-/*
-
-on using an O(n^3) approach we will get ultimately 
-all the triplets possible but 
-{0,1,-1} and {-1,0,1} as well 
-
-so now these are same but set will not recognise them since
-ordering is different
-
-Now optimising to something better near about o(n^2)
-
-Now practcally we are using third loop because we need the sum 
-But is it there really need of it ??
-
-*/
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=n-1;
-
-            while(j<k){
-                long long sum=1LL*(nums[i]+nums[j]+nums[k]);
-                if(sum<0){
-                    j++;
+vector<vector<int>>ans;
+        for(int i=0;i<n-2;i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            int l=i+1;
+            int r=n-1;
+            int rhs=-(nums[i]);
+            while(l<r){
+                int sum=nums[l]+nums[r];
+                if(sum==rhs){
+                    vector<int>temp={nums[i],nums[l],nums[r]};
+                    ans.push_back(temp);
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]){
+                        l++;
+                    }
+                    while(l<r && nums[r]==nums[r+1]){
+                        r--;
+                    }
                 }
-                else if(sum>0){
-                    k--;
+                else if(sum<rhs){
+                    l++;
                 }
                 else{
-                    vector<int>temp={nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
-                    j++;
-                    k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
+                    r--;
                 }
             }
         }
-return ans;  
+        return ans;
     }
 };
