10d2a7f737236e09462b76c726629de21eb0504d
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-10 11:44:19 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -28,9 +28,20 @@ public:
 
         return dp[n];
     }
+    int solveUsingSpacOpt(int n){
+        if(n==0) return 0;
+        int prev1=0;
+        int prev2=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return prev2;
+    }
     int fib(int n) {
         // Step 1: Create a Dp array and pass it into the function 
         vector<int>dp(n+1,-1);
-        return solveUsingTab(n); 
+        return solveUsingSpacOpt(n); 
     }
 };
