491749f55eacc3e064da7ffc8c89cfa8a26cac64
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-10 11:41:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,14 +4,33 @@ public:
         if (n==0 || n==1) return n;
         return solveUsingRec(n-1)+solveUsingRec(n-2);
     }
+    // Stepn 2: Pass into function and copy logic 
+
     int solveUsingMem(int n,vector<int>&dp){
         if (n==0 || n==1) return n;
+        // Step 4: If ans already exists then reutrn it.
         if(dp[n]!=-1) return dp[n];
+        // Step 3: Return the ans in Dp array
         return dp[n]=solveUsingMem(n-1,dp)+solveUsingMem(n-2,dp);
     }   
+    int solveUsingTab(int n){
+        // DP pass 
+        vector<int>dp(n+1,-1);
+        // DP ki state maination 
+        dp[0]=0;
+        if(n==0) return dp[0];
+        dp[1]=1;
+
+        // Then recursion ka reverse
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
     int fib(int n) {
         // Step 1: Create a Dp array and pass it into the function 
         vector<int>dp(n+1,-1);
-        return solveUsingMem(n,dp); 
+        return solveUsingTab(n); 
     }
 };
