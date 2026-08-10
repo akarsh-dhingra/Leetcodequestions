3c8025ce89cd09fbe7fe094cb4e08f39fd661776
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-10 11:32:07 +0530

    Time: 14 ms (22.65%), Space: 7.7 MB (89.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,48 +1,11 @@
 class Solution {
 public:
     int solveUsingRec(int n){
-        if(n==0 || n==1) return n;
-
+        if (n==0 || n==1) return n;
         return solveUsingRec(n-1)+solveUsingRec(n-2);
     }
-    int solveUsingMem(int n,vector<int>&dp){
-         if(n==0 || n==1) return dp[n]= n;
 
-         if(dp[n]!=-1) return dp[n];
-        // step 2: Store the ans in the dp array and return it 
-        return dp[n]= solveUsingRec(n-1)+solveUsingRec(n-2);
-    }
-    int solveUsingTab(int n){
-        // Create a dp array in the fucntion
-        vector<int>dp(n+1,-1);
-
-        // Analyse the base case and update dp array accordingly
-        dp[0]=0;
-        if(n==0) return 0;
-        dp[1]=1;
-        // Reverse the param range and update the arry accordingly 
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-    int solveUsingSpaceOpt(int n){
-        if(n==0) return 0;
-        if(n==1) return 1;
-
-        int prev1=0;
-        int prev2=1;
-        for(int i=2;i<=n;i++){
-            int curr=prev1+prev2;
-            prev1=prev2;
-            prev2=curr;
-        }
-        return prev2;
-    }
     int fib(int n) {
-        // Step1 : Create a DP array and pass it into the function
-
-        
-        return solveUsingSpaceOpt(n);
+    return solveUsingRec(n); 
     }
 };
