450d7bfee8f050cde6869440e3e4d0c87eeebdbc
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-10 11:37:55 +0530

    Time: 3 ms (64.78%), Space: 8.1 MB (17.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,8 +4,14 @@ public:
         if (n==0 || n==1) return n;
         return solveUsingRec(n-1)+solveUsingRec(n-2);
     }
-
+    int solveUsingMem(int n,vector<int>&dp){
+        if (n==0 || n==1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solveUsingMem(n-1,dp)+solveUsingMem(n-2,dp);
+    }   
     int fib(int n) {
-    return solveUsingRec(n); 
+        // Step 1: Create a Dp array and pass it into the function 
+        vector<int>dp(n+1,-1);
+        return solveUsingMem(n,dp); 
     }
 };
