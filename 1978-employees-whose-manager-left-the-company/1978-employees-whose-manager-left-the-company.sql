a9d77270e026695a7c43582fbe58070ad4610337
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-11 19:23:19 +0530

    Time: 320 ms (96.34%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -4,5 +4,5 @@ SELECT e.employee_id
 FROM employees as e
 WHERE e.salary <30000
 AND e.manager_id IS NOT NULL
-AND NOT EXISTS (SELECT 1 from EMPLOYEES m WHERE m.employee_id = e.manager_id )
+AND NOT EXISTS (SELECT m.manager_id from EMPLOYEES m WHERE m.employee_id = e.manager_id )
 ORDER BY e.employee_id
