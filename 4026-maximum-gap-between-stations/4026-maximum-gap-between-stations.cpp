969c65f758f009798e236c650985a7aa9e26ebd6
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-17 08:54:59 +0530

    Time: 44 ms (86.87%), Space: 72.2 MB (81.11%) - LeetHub

--- 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.cpp
+++ 4026-maximum-gap-between-stations/4026-maximum-gap-between-stations.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int maximumGap(string skill, string station) {
-        // har ak worker ko ak distinct station par rkhna hai !!
-        int n=skill.size();
-        int m=station.size();
-
-        vector<int>left(n,-1);
+        int n=skill.size(); 
+        if(n==1) return 0;
+        int m=station.size(); 
+        vector<int>left(n,-1); 
         int l=0;
         int r=0;
-        while(l<n){
+        while(l<n && r<m){
             if(skill[l]==station[r]){
                 left[l]=r;
                 l++;
@@ -18,28 +17,23 @@ public:
                 r++;
             }
         }
-        vector<int>right(n,-1);
         l=n-1;
         r=m-1;
+        vector<int>right(n,-1); 
         while(r>=0 && l>=0){
             if(skill[l]==station[r]){
-            right[l]=r;
-            l--;
-            r--;
+                right[l]=r;
+                l--;
+                r--;
             }
             else{
                 r--;
             }
         }
-        int ans=0;
-        for(int i=1; i<n; i++){
-            ans = max(ans, right[i] - left[i-1]);
+        int maxi=0;
+        for(int i=1;i<n;i++){
+            maxi=max(maxi,right[i]-left[i-1]);
         }
-
-    return ans;
-
-
-        // station[j]==skill[i];
-        
+        return maxi;
     }
 };
