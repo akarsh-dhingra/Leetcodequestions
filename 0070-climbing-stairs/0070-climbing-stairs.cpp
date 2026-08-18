56fcc6848aa7c2e22c286d67157818d5bf2bdd63
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-18 14:02:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (39.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -13,9 +13,18 @@ public:
         int secondWay=solveUsingMem(n-2,dp);
         return dp[n]=firstWay+secondWay;
     }
+    int solveUsingTab(int n){
+        vector<int>dp(n+1,-1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     int climbStairs(int n) {
         // 1. Create a dp arrya of size n
         vector<int>dp(n+1,-1);
-        return solveUsingMem(n,dp);
+        return solveUsingTab(n);
     }
 };
