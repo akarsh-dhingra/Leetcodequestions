715f3ae937c9e5cbb283a9a3b975b8747f755890
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-18 14:54:33 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.38%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -20,9 +20,16 @@ public:
         int secondWay=cost[start]+solveUsingMem(cost,start+2,n,dp);
         return dp[start]=min(firstWay,secondWay);
     }
+    int solveUsingTab(vector<int>& cost,int n){
+            vector<int>dp(n+2,0);
+            for(int i=n-1;i>=0;i--){
+                dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+            }
+            return min(dp[0],dp[1]);
+    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
         vector<int>dp(n+1,-1);
-        return min(solveUsingMem(cost,0,n,dp),solveUsingMem(cost,1,n,dp));
+        return solveUsingTab(cost,n);
     }
 };
