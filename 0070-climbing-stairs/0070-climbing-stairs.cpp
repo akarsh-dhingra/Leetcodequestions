f579af6f423e93c7081216cfff6c3b11359971d8
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-18 14:01:31 +0530

    Time: 2 ms (5.03%), Space: 8.5 MB (39.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,38 +1,21 @@
 class Solution {
-    public:
+public:
     int solveUsingRec(int n){
-        if(n==0||n==1) return 1;
-        int oneStep=solveUsingRec(n-1);
-        int twoStep=solveUsingRec(n-2);
-        return oneStep+twoStep;
-     }
-     int solveUsingMem(vector<int>&dp,int n){
-        if(n==0||n==1) return dp[n]=1;
+        if(n==1|| n==0) return 1;
+        int firstWay=solveUsingRec(n-1);
+        int secondWay=solveUsingRec(n-2);
+        return firstWay+secondWay;
+    }
+    int solveUsingMem(int n,vector<int>&dp){
+        if(n==1|| n==0) return 1;
         if(dp[n]!=-1) return dp[n];
-        int oneStep=solveUsingMem(dp,n-1);
-        int twoStep=solveUsingMem(dp,n-2);
-        return dp[n]=oneStep+twoStep;
-     }
-     int solveUsingTab(int n){
-        vector<int>dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-     }
-     int solveUsingSpaceOpt(int n){
-            int prev1=1;
-            int prev2=1;
-            for(int i=2;i<=n;i++){
-                int curr=prev1+prev2;
-                prev1=prev2;
-                prev2=curr;
-            }
-            return prev2;
+        int firstWay=solveUsingMem(n-1,dp);
+        int secondWay=solveUsingMem(n-2,dp);
+        return dp[n]=firstWay+secondWay;
     }
     int climbStairs(int n) {
-        return solveUsingSpaceOpt(n);
+        // 1. Create a dp arrya of size n
+        vector<int>dp(n+1,-1);
+        return solveUsingMem(n,dp);
     }
 };
