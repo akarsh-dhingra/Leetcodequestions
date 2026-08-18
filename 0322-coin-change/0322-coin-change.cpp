7d1229a47aafccbe6359a51eefffd5c6a6959d7c
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-18 20:54:08 +0530

    Time: 27 ms (63.76%), Space: 21.9 MB (52.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -29,11 +29,23 @@ public:
         }
         return dp[amount]=mini;
     }
+    int solveUsingTab(vector<int>& coins, int amount ){
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX){
+                    dp[i]=min(dp[i],1+dp[i-coins[j]]);
+                }
+            }
+        }
+        if(dp[amount]==INT_MAX) return -1;
+        else return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<int>dp(amount+1,-1);
-        int ans= solveUsingMem(dp,coins,amount);
-        if(ans==INT_MAX) return -1;
-        else return ans;
+        int ans= solveUsingTab(coins,amount);
+        return ans;
     }
 };
