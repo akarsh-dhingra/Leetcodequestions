39dfa42455ca1ba88ffb8c6ea2009d3ca797d32c
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-19 21:29:44 +0530

    Time: 11 ms (35.11%), Space: 23.1 MB (21.93%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,26 +1,24 @@
 class Solution {
-public: 
-    double hoursTaken(int k,vector<int>& piles){
-        double ghante=0;
-        for(int i=0;i<piles.size();i++){
-                ghante+=ceil((double)piles[i]/k);
+public:
+    double timeTaketoEat(vector<int>& piles,int t){
+        double time=0;
+        for(int x:piles){
+            time+=ceil((double)x/t);
         }
-        return ghante;
+        return time;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        // k=1 to max number of bananas 
-        int  low=1;
-        int  high=*max_element(piles.begin(),piles.end());
-        int  ans=0;
+        int n=piles.size();
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+int ans=-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(hoursTaken(mid,piles)<=h){
+            int mid=low+(high-low)/2;
+            if(timeTaketoEat(piles,mid)<=h){
                 ans=mid;
                 high=mid-1;
             }
             else{
-                // aur tez kha agr 30 par bat nahi bn rhi bnan rjhi toh aur tez kha 
-                // tera misconception 
                 low=mid+1;
             }
         }
