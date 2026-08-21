1316304e1c8a92620a0c0fa7c269f3e7d1027663
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-21 10:06:17 +0530

    Time: 27 ms (5%), Space: 189.7 MB (12.22%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,31 +10,30 @@
  */
 class Solution {
 public:
-    ListNode *reverseList(ListNode* head){
+    ListNode* reverseList(ListNode* head){
         if(head==NULL || head->next==NULL) return head;
         ListNode*temp=head;
-        ListNode*agla=reverseList(temp->next);
+        ListNode*agle=reverseList(temp->next);
         temp->next->next=temp;
         temp->next=NULL;
-        return agla;
+        return agle;
     }
     ListNode* swapNodes(ListNode* head, int k) {
-     ListNode*x1=head;
-     ListNode*temp=head;
-     int cnt=1;
-     while(cnt<k ){
-        x1=x1->next;
-        cnt++;
-     }
-     ListNode*reverseHead=reverseList(head);
-     ListNode *x2=reverseHead;
-     cnt=1;
-     while(cnt<k ){
-        x2=x2->next;
-        cnt++;
-     }
-    reverseList(reverseHead);
-    swap(x1->val,x2->val);
-    return head;    
+        ListNode*x1=head;
+        int cnt=1;
+        while(cnt<k){
+            x1=x1->next;
+            cnt++;
+        }
+        ListNode*x2=reverseList(head);
+        ListNode*newHead=x2;
+        cnt=1;
+        while(cnt<k){
+            x2=x2->next;
+            cnt++;
+        }
+        reverseList(newHead);
+        swap(x1->val,x2->val);
+        return head;
     }
 };
