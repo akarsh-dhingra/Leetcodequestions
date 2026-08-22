ef337a3f6bc2feefaa47bef1469548b93433b4e0
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-22 09:29:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.17%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    int findSum(int n){
+    bool checkDivisibility(int n) {
         int sum=0;
+        int prod=1;
+        int origin=n;
         while(n!=0){
             int temp=n%10;
             sum+=temp;
-            n/=10;
-        }
-        return sum;
-    }
-    int findProd(int n){
-        int prod=1;
-        while(n!=0){
-            int temp=n%10;
             prod*=temp;
             n/=10;
         }
-        return prod;
-    }
-    bool checkDivisibility(int n) {
-        int sum=findSum(n);
-        int prod=findProd(n);
-        int x=sum+prod;
-        if(n%x==0) return true;
-        else return false;
+        return origin%(sum+prod)==0;
     }
 };
