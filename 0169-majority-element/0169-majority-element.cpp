c1012b7cf27590e21328b2d7f7981b85b32978f7
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-23 21:12:01 +0530

    Time: 0 ms (100%), Space: 28.4 MB (14.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-        // BOYER MOORE VOTING ALGO:
-        // A -> i know it will enoguh no. of times that
-        // is greater than n/2 to be a majority 
-        // and other candidates wll keep cancelling 
-        // and if at any it's cnt value reaches to 0
-        // I can surely say that since it was balanced out by 
-        // equal no. of elements thus we cannot say that it is.
-// Every time a cnt of my elements if it 
-// reaches to 0 I"ll change my majority el to a newer value why?? Because uptill this 
-// point I am not sure that whether this element is a majority or not since it was cancelled out by equal no. of elements so that is why I am kind of you can say starting a new journey of cnting now here cnt is not denoting the actual cnt value but it is just a simple variable depicting us the no. of votes that are there in majority for a particular el.
-// 
-       int cnt=0;
-       int n=nums.size();
-       int el;
-        for(int i=0;i<n;i++){
-            
-            if(cnt==0) {
-                el=nums[i];
-                cnt++;
-            }
-            else if(el==nums[i]){
-                cnt++;
-            }
-            else {
-                cnt--;
-            }
+        unordered_map<int,int>mpp;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
         }
-
-        int occ=0;
+        int val=ceil(n/2);
+        int ans=-1;
         for(int i=0;i<n;i++){
-            if(el==nums[i]) occ++;
+            if(mpp[nums[i]]>val) ans=nums[i];
         }
-        int ans=0;
-        if(occ>n/2 && cnt>0) ans= el;
         return ans;
-
-        }
+    }
     };
