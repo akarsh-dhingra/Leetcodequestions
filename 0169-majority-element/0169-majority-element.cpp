522948dc59bfbe94e6d2928b865a8dc43236d086
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-23 21:23:34 +0530

    Time: 0 ms (100%), Space: 28.4 MB (14.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
         int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        int val=ceil(n/2);
-        int ans=-1;
+        int el=0;
+        int cnt=0;
+
         for(int i=0;i<n;i++){
-            if(mpp[nums[i]]>val) ans=nums[i];
+            if(cnt==0){
+                el=nums[i];
+                cnt++;
+            }
+            else if(el==nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
         }
-        return ans;
+        return el;
     }
     };
