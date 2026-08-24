12bdecf2a1ccc6599042cf3c75a5f4ef2ccd1804
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-24 13:00:55 +0530

    Time: 485 ms (72.14%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,7 @@
 SELECT t.id
 FROM Weather as t
 JOIN Weather as y
-ON t.recordDate=y.recordDate+INTERVAL 1 DAY
-WHERE t.temperature>y.temperature
+ON t.recordDate=y.recordDate + INTERVAL 1 DAY 
+where t.temperature> y.temperature
+
+
