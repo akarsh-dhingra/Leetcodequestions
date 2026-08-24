b64824c3e93d84cbcbaeb49864c3037d280af467
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-24 22:37:35 +0530

    Time: 293 ms (45.13%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,4 @@
-SELECT c.id,c.movie,c.description,c.rating
-from Cinema as c
-Where c.description !='boring' AND c.id%2!=0
-ORDER BY c.rating DESC
+SELECT *
+FROM Cinema as c 
+WHERE c.id %2!=0 AND c.description!="boring"
+order by c.rating DESC
