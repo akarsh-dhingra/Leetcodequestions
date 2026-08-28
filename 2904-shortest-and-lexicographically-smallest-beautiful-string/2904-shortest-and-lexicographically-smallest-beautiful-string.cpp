d03e596629ffa4c5974fefb09ca6b50f2f51c28e
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-28 12:45:53 +0530

    Time: 7 ms (11.58%), Space: 11.3 MB (5.06%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n=s.length();
-        vector<string>ans;  
-        int l=0;
-        int r=0;
-        int cnt=0;
-        string best="";
-        int minLen=INT_MAX;
-        while(r<n){
-            if(s[r]=='1') cnt++;
-            while(cnt==k){
-                int len=r-l+1;
-                string candidate=s.substr(l,len);
-                if(candidate.length()<minLen){
-                    best=candidate;
-                    minLen=candidate.length();
-                }
-                else if(candidate.length() == minLen && candidate < best){
-                    best=candidate;
-                }
-                if(s[l]=='1') cnt--;
-                l++;
-            }
-            r++;
-        }
 
-        return best;
+        for(int len=k;len<=n;len++){
+            string result="";
+            for(int i=0;i<=n-len;i++){
+                string temp=s.substr(i,len);
+                if(ranges::count(temp,'1')==k){
+                    if(result.empty() || temp<result){
+                        result=temp;
+                    }
+                }
+            }
+            if(!result.empty()) return result;
+        }
+        return "";
     }
 };
