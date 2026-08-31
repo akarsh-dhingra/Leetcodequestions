2d6cbb2f74c9925d43a4fa10f246bfb6c1e4db23
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-08-31 11:28:52 +0530

    Time: 7 ms (11.1%), Space: 92.2 MB (4.9%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        // Solve without finding mini and maxi ??
         int n=nums.size();
-        int maxi=*max_element(nums.begin(),nums.end());
-        int mini=*min_element(nums.begin(),nums.end());
-        int first=-1;
-        int second=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==maxi){
-                first=i;
-            }
-            if(nums[i]==mini){
-                second=i;
-            }
-        }
-        int ans1=0;
-        if(second>first){
-            ans1+=(n-second);
-            ans1+=(first+1);
-        }
-        else{
-            ans1+=(second+1);
-            ans1+=(n-first);
-        }
-        int ans2=0;
-        if(second>=first) ans2+=(second+1);
-        else ans2+=(first+1);
-        int ans3=0;
-        if(second>=first) ans3+=(n-first);
-        else ans3+=(n-second);
+        int mini=min_element(nums.begin(),nums.end())-begin(nums);
+        int maxi=max_element(nums.begin(),nums.end())-begin(nums);
 
-        return min(ans1,min(ans2,ans3));
+        int left=min(mini,maxi);
+        int right=max(mini,maxi);
+
+
+        return min({left+1+n-right,right+1,n-left});
     }
 };
