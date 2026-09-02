43b8dd05ab1d702d67dc41a378e8e6f95af3f962
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-09-02 23:57:52 +0530

    Time: 0 ms (100%), Space: 30.2 MB (94.95%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // we have to return true if it is possible to construct 
+        // such kind of an array    
+        return true;
+    }
+};
