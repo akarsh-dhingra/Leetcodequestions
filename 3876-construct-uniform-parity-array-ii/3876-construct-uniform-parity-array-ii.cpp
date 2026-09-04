c6b4b1c7d0f5222266135874239ab0294dbe2552
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-09-04 13:07:53 +0530

    Time: 0 ms (100%), Space: 165.9 MB (25.16%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int checkAllOddOrEven(vector<int>& nums1){
-        int n=nums1.size();
-        int cnt1=0;
-        int cnt2=0;
-        for(int x:nums1){
-            if(x%2==0) cnt1++;
-            else cnt2++;
-        }
-        if(cnt1==n || cnt2==n) return true;
-        else return false;
-    }
+    // int checkAllOddOrEven(vector<int>& nums1){
+    //     int n=nums1.size();
+    //     int cnt1=0;
+    //     int cnt2=0;
+    //     for(int x:nums1){
+    //         if(x%2==0) cnt1++;
+    //         else cnt2++;
+    //     }
+    //     if(cnt1==n || cnt2==n) return true;
+    //     else return false;
+    // }
     bool uniformArray(vector<int>& nums1) {
     //    All even bnana kabhi possible nahi hai
     // Tabhi bna skte ho agr aapke paas all even and no odd ho toh uss case
     //  mai only possible hai varna nahi hai it's not at all possible otherwise.
     int mini=*min_element(nums1.begin(),nums1.end());
     if(mini%2!=0) return true;
-    if(checkAllOddOrEven(nums1)) return true;
-    return false;
+    for(int x:nums1){
+        if(x%2==1) return false;
+    }
+    return true;
     }
 };
