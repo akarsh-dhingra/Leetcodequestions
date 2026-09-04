1ab15c5f2cd68e1dcc38c285fc9f67143048b011
Akarsh_01 <167018667+akarsh-dhingra@users.noreply.github.com>
2026-09-04 11:45:51 +0530

    Time: 7 ms (78.08%), Space: 202.6 MB (84.96%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+         int n=nums.size();
+        int l=0;
+        int maxi=INT_MIN;
+        // One approach that you can use 
+        // is preProcessing that you preprocess all the mini Pehle sa hee till 
+        // a specific index 
+        
+        vector<int>miniFromIndx(n,1e9);
+        miniFromIndx[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            miniFromIndx[i]=min(nums[i],miniFromIndx[i+1]);
+        } 
+        while(l<n){
+            maxi=max(nums[l],maxi);
+            int mini=miniFromIndx[l];
+            if(maxi-mini<=k){
+                return l;
+            }
+            l++;
+        }
+    return -1;
+    }
+};
